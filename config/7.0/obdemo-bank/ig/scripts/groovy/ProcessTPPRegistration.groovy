1e3fd1b12563f5650cf3a45a84229901fa61c3ec
christian-brindley <christian.brindley@forgerock.com>
2021-02-26 00:48:24 +0000

    23: IG - convert java filters to groovy scripts (#17)
    Resolves: https://github.com/SecureBankingAcceleratorToolkit/SecureBankingAcceleratorToolkit/issues/23

--- config/7.0/obdemo-bank/ig/scripts/groovy/ProcessTPPRegistration.groovy
+++ config/7.0/obdemo-bank/ig/scripts/groovy/ProcessTPPRegistration.groovy
@@ -1,7 +1,4 @@
 import org.forgerock.http.protocol.*
-import org.forgerock.json.jose.utils.Utils
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
 import org.forgerock.json.jose.*
 import org.forgerock.json.jose.common.JwtReconstruction
 import org.forgerock.json.jose.jws.SignedJwt
@@ -14,6 +11,27 @@ import org.forgerock.json.jose.jws.SignedJwt
 
 
 def error = false
+
+def SCOPE_ACCOUNTS = "accounts"
+def SCOPE_PAYMENTS = "payments"
+
+
+def ROLE_PAYMENT_INITIATION             = "0.4.0.19495.1.2"
+def ROLE_ACCOUNT_INFORMATION            = "0.4.0.19495.1.3"
+def ROLE_CARD_BASED_PAYMENT_INSTRUMENTS = "0.4.0.19495.1.4"
+
+
+// Check we have everything we need from the client certificate
+
+if (!attributes.clientCertificate) {
+    logger.error("No client certificate for TPP registration")
+    return new Response(Status.BAD_REQUEST)
+}
+
+if (!attributes.clientCertificate.roles) {
+    logger.error("No roles in client certificate for TPP registration")
+    return new Response(Status.BAD_REQUEST)
+}
 
 // Parse incoming registration JWT
 
@@ -29,6 +47,11 @@ def oidcRegistration = regJwt.getClaimsSet();
 
 def ssa = oidcRegistration.getClaim("software_statement", String.class);
 
+if (!ssa) {
+    logger.error("No SSA")
+    return new Response(Status.BAD_REQUEST)
+}
+
 logger.debug("Got ssa [" + ssa + "]")
 
 def ssaJwt = new JwtReconstruction().reconstructJwt(ssa,SignedJwt.class)
@@ -40,6 +63,11 @@ def tppName = ssaClaims.getClaim("software_client_name", String.class);
 def tppCertId = ssaClaims.getClaim("org_id", String.class);
 def tppJwksUri = ssaClaims.getClaim("software_jwks_endpoint");
 
+logger.debug("Inbound TPP details from SSA: tppName: {} tppCertId: {} tppJwksUri: {}",
+        tppName,
+        tppCertId,
+        tppJwksUri
+)
 
 // Update OIDC registration request
 
@@ -47,9 +75,54 @@ oidcRegistration.setClaim("jwks_uri",tppJwksUri)
 oidcRegistration.setClaim("client_name",tppName)
 oidcRegistration.setClaim("tls_client_certificate_bound_access_tokens", true)
 
-// TODO: Map scopes to roles
+// Sanity check on scopes
+
+def scopes = oidcRegistration.getClaim("scope")
+def roles = attributes.clientCertificate.roles
+
+if (scopes.contains(SCOPE_ACCOUNTS) && !(roles.contains(ROLE_ACCOUNT_INFORMATION))) {
+    logger.error("Requested scope {} requires certificate role {}",
+            SCOPE_ACCOUNTS,
+            ROLE_ACCOUNT_INFORMATION
+    )
+    return new Response(Status.FORBIDDEN)
+}
+
+if (scopes.contains(SCOPE_PAYMENTS) && !(roles.contains(ROLE_PAYMENT_INITIATION))) {
+    logger.error("Requested scope {} requires certificate role {}",
+            SCOPE_PAYMENTS,
+            ROLE_PAYMENT_INITIATION
+    )
+    return new Response(Status.FORBIDDEN)
+}
+
+// Cross check TPP ID with cert
+//
+// e.g. PSDGB-FFA-5f563e89742b2800145c7da1
+
+def  organizationalIdentifier = attributes.clientCertificate.subjectDNComponents.OI
+
+if (!organizationalIdentifier) {
+    logger.error("No organizational identifier in cert")
+    return new Response(Status.FORBIDDEN)
+}
+
+def oiComponents = organizationalIdentifier.split("-")
+
+if (oiComponents.length != 3) {
+    logger.error("Wrong number of dashes in OI {} - expected 2",organizationalIdentifier)
+    return new Response(Status.FORBIDDEN)
+}
+
+def dnId = oiComponents[2]
+
+if (dnId != tppCertId) {
+    logger.error("TPP ID in cert {} does not match id in SSA {}",dnId,tppCertId)
+    return new Response(Status.FORBIDDEN)
+}
+
 // TODO: Subject DN for cert bound access tokens
-// TODO: Cross check software id with transport cert
+
 
 // Convert to JSON and pass it on
 
