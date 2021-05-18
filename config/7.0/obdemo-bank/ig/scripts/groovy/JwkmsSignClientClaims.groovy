4ca45866bbafc7cdc70ae43a3e8cc35a93706ae1
christian-brindley <christian.brindley@forgerock.com>
2021-05-18 10:52:51 +0100

    47: Detached signature - allow unencoded signature payload (#27)
    Resolves: https://github.com/SecureBankingAccessToolkit/SecureBankingAcceleratorToolkit/issues/47

--- config/7.0/obdemo-bank/ig/scripts/groovy/JwkmsSignClientClaims.groovy
+++ config/7.0/obdemo-bank/ig/scripts/groovy/JwkmsSignClientClaims.groovy
@@ -11,6 +11,12 @@ import org.forgerock.json.jose.jws.SigningManager;
 import org.forgerock.json.jose.jws.handlers.SigningHandler;
 import java.security.PrivateKey
 
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.*;
+import com.nimbusds.jose.jwk.*;
+
+import groovy.json.JsonSlurper
+
 
 logger.debug("Signing claims as ApiClient")
 
@@ -27,24 +33,81 @@ if (!attributes.clientCertificate.privateKey) {
     return new Response(Status.BAD_REQUEST)
 }
 
-def jwtClaims = new JwtClaimsSet(request.entity.getJson())
+def optionsHeaders = request.headers.get(routeArgOptionsHeader)
+
+def signingOptions = []
+
+if (optionsHeaders != null) {
+
+    String optionsJson = URLDecoder.decode(optionsHeaders.firstValue.toString())
+    logger.debug("Got options header " + optionsJson)
+    def slurper = new JsonSlurper()
+    signingOptions = slurper.parseText(optionsJson)
+    if (signingOptions == null) {
+        logger.error("Couldn't parse signing options")
+        return new Response(Status.BAD_REQUEST)
+    }
+}
+
 
 PrivateKey signingKey = attributes.clientCertificate.privateKey;
 
 logger.debug("Recovered private key {}",signingKey)
 
-SigningHandler signingHandler = new SigningManager().newRsaSigningHandler(signingKey);
-
+def jwt = ""
+def isDetached = (signingOptions && signingOptions.detachedsig == true)
 def kid = attributes.clientCertificate.serialNumber.toString()
 
-def jwt = new JwtBuilderFactory()
-                .jws(signingHandler)
-                .headers()
-                .alg(JwsAlgorithm.PS256)
-                .kid(kid)
-                .done()
-                .claims(jwtClaims)
-                .build();
+if (signingOptions.unencodedpayload) {
+
+    Payload detachedPayload = new Payload(request.entity.getString());
+
+    JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.PS256)
+            .base64URLEncodePayload(false)
+            .criticalParams(Collections.singleton("b64"))
+            .keyID(kid)
+            .build();
+
+    JWSObject jwsObject = new JWSObject(header, detachedPayload);
+    // jwsObject.sign(new MACSigner(hmacJWK));
+    jwsObject.sign(new RSASSASigner(signingKey));
+
+    jwt = jwsObject.serialize(isDetached);
+}
+else {
+    def jwtClaims = new JwtClaimsSet(request.entity.getJson())
+
+    SigningHandler signingHandler = new SigningManager().newRsaSigningHandler(signingKey);
+
+
+
+    jwt = new JwtBuilderFactory()
+            .jws(signingHandler)
+            .headers()
+            .alg(JwsAlgorithm.PS256)
+            .kid(kid)
+            .done()
+            .claims(jwtClaims)
+            .build();
+
+    if (isDetached) {
+        String[] jwtElements = jwt.split("\\.")
+
+        if (jwtElements.length != 3) {
+            logger.error("Wrong number of dots on generated jwt " +  jwt.length)
+            Response response = new Response(Status.INTERNAL_SERVER_ERROR)
+            return response
+        }
+
+        jwt = jwtElements[0] + ".." + jwtElements[2]
+
+        logger.debug("Removed payload - now {}",jwt)
+    }
+}
+
+logger.debug("Generated jwt {}", jwt)
+
+
 
 Response response = new Response(Status.OK)
 response.getHeaders().add("Content-Type","text/plain");
--- config/7.0/obdemo-bank/ig/scripts/groovy/ProcessDetachedSig.groovy
+++ config/7.0/obdemo-bank/ig/scripts/groovy/ProcessDetachedSig.groovy
@@ -2,11 +2,70 @@ import org.forgerock.http.protocol.*
 import org.forgerock.json.jose.*
 import org.forgerock.json.jose.jwk.store.JwksStore.*
 import org.forgerock.json.JsonValueFunctions.*
+import groovy.json.JsonSlurper
+
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.*;
+import com.nimbusds.jose.jwk.*;
+
+def validateUnencodedPayload(jws, payload) {
+    Payload detachedPayload = new Payload(payload);
+    JWSObject parsedJWSObject = JWSObject.parse(jws, detachedPayload);
+    JWKSet jwkSet
+    try {
+        jwkSet = JWKSet.load(new URL(routeArgJwkUrl))
+    }
+    catch (e) {
+        logger.error("Exception getting JWK set; {}",e)
+        return false
+    }
+
+    JWSHeader jwsHeader = parsedJWSObject.getHeader();
+    List<JWK> matches = new JWKSelector(JWKMatcher.forJWSHeader(jwsHeader))
+            .select(jwkSet);
+
+
+    if (matches.size() != 1) {
+        logger.error("Unexpected number of matching JWKs - {}",matches.size())
+        return false
+    }
+
+    RSAKey rsaPublicJWK = matches.get(0).toPublicJWK();
+
+    logger.debug("Got public key {}",rsaPublicJWK)
+
+    JWSVerifier verifier = new RSASSAVerifier(rsaPublicJWK);
+    return parsedJWSObject.verify(verifier)
+}
 
 // On the way in, build a JWT from the header and content to pass to the JWT verification filter
+//
+// Subject to waiver for earlier versions as per
+// https://openbanking.atlassian.net/wiki/spaces/DZ/pages/1112670669/W007
+//
+// If ASPSPs are still using v3.1.3 or earlier, they must support the parameter b64 to be false,
+// and any TPPs using these ASPSPs must do the same.
+//
+// If ASPSPs have updated to v3.1.4 or later, they must not include the b64 claim in the header,
+// and any TPPs using these ASPSPs must do the same.
+
+def splitPath = request.uri.path.split("/")
+
+if (splitPath.length < 2) {
+    logger.error("Can't parse API version for inbound request")
+    return new Response(Status.BAD_REQUEST)
+}
+
+def apiVersion = splitPath[splitPath.length - 2]
+
 
 logger.debug("Building JWT from detached header")
-def header = request.headers.get(_arg_headerName)
+
+logger.debug("API version " + apiVersion)
+
+
+
+def header = request.headers.get(routeArgHeaderName)
 
 if (header == null) {
     logger.error("No detached signature header on inbound request " + _arg_headerName)
@@ -27,18 +86,47 @@ if (sigElements.length != 3) {
 
 String jwtHeader      = sigElements[0]
 String jwtSignature   = sigElements[2]
-String jwtPayload     = request.entity.bytes.encodeBase64Url().toString()
 
-String jwt = jwtHeader + "." + jwtPayload + "." + jwtSignature
+// Check JWT header for b64 claim
+//
+// If claim is present, and API version > 3.1.3 then reject
+// If claim is present, and is set to false, and API < 3.1.4 then accept and validate as non base64 payload
 
-logger.debug("Constructed JWT " + jwt)
+String headerJson = new String(jwtHeader.decodeBase64Url())
+logger.debug("Got JWT header " + headerJson)
+JsonSlurper slurper = new JsonSlurper()
+def headerObj = slurper.parseText(headerJson)
 
-attributes.detachedJWT = jwt
+String jwtPayload = null
+
+
+if (headerObj.b64 != null && apiVersion > "v3.1.3") {
+    logger.error("B64 header not permitted in JWT header after 3.1.3")
+    Response response = new Response(Status.BAD_REQUEST)
+    return response
+}
+else if (headerObj.b64 != null && headerObj.b64 == false) {
+    logger.debug("Unencoded payload")
+    jwtPayload     = request.entity.getString()
+    attributes.encodedPayload = false
+    if (!validateUnencodedPayload(detachedSig,jwtPayload)) {
+        logger.error("Signature validation failed")
+        Response response = new Response(Status.UNAUTHORIZED)
+        return response
+    }
+    logger.debug("Detached signature verified against unencoded payload")
+}
+else {
+    logger.debug("Standard base64 encoded payload for detached sig")
+    jwtPayload     = request.entity.bytes.encodeBase64Url().toString()
+    attributes.encodedPayload = true
+    String jwt = jwtHeader + "." + jwtPayload + "." + jwtSignature
+
+    logger.debug("Constructed JWT " + jwt)
+
+    attributes.detachedJWT = jwt
+}
+
 
 next.handle(context,request)
-
-
-
-
-
 
