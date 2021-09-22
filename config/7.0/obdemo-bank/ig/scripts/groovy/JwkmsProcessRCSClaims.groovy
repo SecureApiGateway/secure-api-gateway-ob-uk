3350a8588439f80470d3f1c65def4ec07a42bc69
christian-brindley <christian.brindley@forgerock.com>
2021-09-22 21:53:24 +0100

    87: RCS signing service in IG (#59)
    Resolves: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/87

--- config/7.0/obdemo-bank/ig/scripts/groovy/JwkmsProcessRCSClaims.groovy
+++ config/7.0/obdemo-bank/ig/scripts/groovy/JwkmsProcessRCSClaims.groovy
@@ -4,9 +4,21 @@ import org.forgerock.json.jose.jwk.store.JwksStore.*
 import org.forgerock.json.JsonValueFunctions.*
 
 
-logger.debug("Processing JWKMS request")
+logger.debug("Signing claims as RCS")
 
-def payload = request.entity.getJson()
+// response object
+response = new Response(Status.OK)
+response.headers['Content-Type'] = "application/json"
+
+def payload = request.entity.getJson();
+
+if (!payload) {
+    message = "Couldn't parse request JSON"
+    logger.error(message)
+    response.status = Status.BAD_REQUEST
+    response.entity = "{ \"error\":\"" + message + "\"}"
+    return response
+}
 
 def iat = new Date().getTime() / 1000;
 
