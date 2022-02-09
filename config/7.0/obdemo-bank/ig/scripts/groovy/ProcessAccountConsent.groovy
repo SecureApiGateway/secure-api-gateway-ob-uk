cf004706cdba91ab2fecd021312c03a8f25449d3
Andra <andra.racovita@itsmartsystems.eu>
2022-02-09 11:14:23 +0200

    Update ProcessAccountConsent.groovy

--- config/7.0/obdemo-bank/ig/scripts/groovy/ProcessAccountConsent.groovy
+++ config/7.0/obdemo-bank/ig/scripts/groovy/ProcessAccountConsent.groovy
@@ -12,7 +12,7 @@ def oauth2ClientId = contexts.oauth2.accessToken.info.client_id
 accountIntentData = request.entity.getJson()
 
 def tz = TimeZone.getTimeZone("UTC");
-def df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+def df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'X'");
 df.setTimeZone(tz);
 def nowAsISO = df.format(new Date());
 
