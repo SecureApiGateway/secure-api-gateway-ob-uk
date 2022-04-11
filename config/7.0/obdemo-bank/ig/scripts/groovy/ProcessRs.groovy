b677bb8e1a0f6631a4d1ea7b314e31a5f6218488
andra-racovita <69030304+andra-racovita@users.noreply.github.com>
2022-04-11 16:04:52 +0300

    Add Domestic Payments Consents APIs in the RS Discovery (#113)
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/253

--- config/7.0/obdemo-bank/ig/scripts/groovy/ProcessRs.groovy
+++ config/7.0/obdemo-bank/ig/scripts/groovy/ProcessRs.groovy
@@ -7,6 +7,8 @@ next.handle(context, request).thenOnResult(response -> {
     response.entity = response.entity.getString().replace(request.getHeaders().getFirst('X-Host'), request.getHeaders().getFirst('X-Forwarded-Host') + "/rs");
 
     try {
+        JsonValue newEntity = response.entity.getJson();
+
         //Account and Transaction
         JsonValue accountAndTransactionApi = response.entity.getJson().get("Data").get("AccountAndTransactionAPI");
         for (JsonValue value : accountAndTransactionApi) {
@@ -18,9 +20,22 @@ next.handle(context, request).thenOnResult(response -> {
             value.Links.links.add("DeleteAccountAccessConsent", "https://" + request.getHeaders().getFirst('X-Forwarded-Host') + "/rs/open-banking/" + value.Version.asString() + "/aisp/account-access-consents/{ConsentId}");
             value.Links.linkValues.add("https://" + request.getHeaders().getFirst('X-Forwarded-Host') + "/rs/open-banking/" + value.Version.asString() + "/aisp/account-access-consents/{ConsentId}");
         }
-        JsonValue newEntity = response.entity.getJson();
         newEntity.get("Data").remove("AccountAndTransactionAPI");
         newEntity.get("Data").add("AccountAndTransactionAPI", accountAndTransactionApi);
+
+        //Payment Initiation
+        JsonValue paymentInitiationAPI = response.entity.getJson().get("Data").get("PaymentInitiationAPI");
+        for (JsonValue value : paymentInitiationAPI) {
+            //Domestic Payments Consents
+            value.Links.links.add("CreateDomesticPaymentConsent", "https://" + request.getHeaders().getFirst('X-Forwarded-Host') + "/rs/open-banking/" + value.Version.asString() + "/pisp/domestic-payment-consents");
+            value.Links.linkValues.add("https://" + request.getHeaders().getFirst('X-Forwarded-Host') + "/rs/open-banking/" + value.Version.asString() + "/pisp/domestic-payment-consents");
+            value.Links.links.add("GetDomesticPaymentConsent", "https://" + request.getHeaders().getFirst('X-Forwarded-Host') + "/rs/open-banking/" + value.Version.asString() + "/pisp/domestic-payment-consents/{ConsentId}");
+            value.Links.linkValues.add("https://" + request.getHeaders().getFirst('X-Forwarded-Host') + "/rs/open-banking/" + value.Version.asString() + "/pisp/domestic-payment-consents/{ConsentId}");
+            value.Links.links.add("GetDomesticPaymentConsentsConsentIdFundsConfirmation", "https://" + request.getHeaders().getFirst('X-Forwarded-Host') + "/rs/open-banking/" + value.Version.asString() + "/pisp/domestic-payment-consents/{ConsentId}/funds-confirmation");
+            value.Links.linkValues.add("https://" + request.getHeaders().getFirst('X-Forwarded-Host') + "/rs/open-banking/" + value.Version.asString() + "/pisp/domestic-payment-consents/{ConsentId}/funds-confirmation");
+        }
+        newEntity.get("Data").remove("paymentInitiationAPI");
+        newEntity.get("Data").add("paymentInitiationAPI", paymentInitiationAPI);
 
         response.entity = newEntity;
     }
