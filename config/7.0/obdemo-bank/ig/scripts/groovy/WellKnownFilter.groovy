367e9a18afd30ae0fcb902e6aa06f1cebdef4c60
Jamie Bowen <jamie.bowen@forgerock.com>
2022-05-12 11:30:44 +0100

    260: Access authorize endpoint on iam.dev url (#122)
    Issue: https://github.com/securebankingaccesstoolkit/securebankingaccesstoolkit/issues/260

--- config/7.0/obdemo-bank/ig/scripts/groovy/WellKnownFilter.groovy
+++ config/7.0/obdemo-bank/ig/scripts/groovy/WellKnownFilter.groovy
@@ -2,16 +2,17 @@ next.handle(context, request).thenOnResult(response -> {
 
   def rspJson = response.entity.getJson();
 
-  rspJson.introspection_endpoint = rspJson.introspection_endpoint.replace(routeArgInternalUri,routeArgExternalUri)
-  rspJson.check_session_iframe   = rspJson.check_session_iframe.replace(routeArgInternalUri,routeArgExternalUri)
   rspJson.issuer                 = rspJson.issuer.replace(routeArgInternalUri,routeArgExternalUri)
-  rspJson.authorization_endpoint = rspJson.authorization_endpoint.replace(routeArgInternalUri,routeArgExternalUri)
   rspJson.token_endpoint         = rspJson.token_endpoint.replace(routeArgInternalUri,routeArgExternalUri)
-  rspJson.end_session_endpoint   = rspJson.end_session_endpoint.replace(routeArgInternalUri,routeArgExternalUri)
-  rspJson.revocation_endpoint    = rspJson.revocation_endpoint.replace(routeArgInternalUri,routeArgExternalUri)
-  rspJson.userinfo_endpoint      = rspJson.userinfo_endpoint.replace(routeArgInternalUri,routeArgExternalUri)
   rspJson.jwks_uri               = rspJson.jwks_uri.replace(routeArgInternalUri,routeArgExternalUri)
   rspJson.registration_endpoint  = rspJson.registration_endpoint.replace(routeArgInternalUri,routeArgExternalUri)
+
+//  rspJson.authorization_endpoint = rspJson.authorization_endpoint.replace(routeArgInternalUri,routeArgExternalUri)
+//  rspJson.introspection_endpoint = rspJson.introspection_endpoint.replace(routeArgInternalUri,routeArgExternalUri)
+//  rspJson.check_session_iframe   = rspJson.check_session_iframe.replace(routeArgInternalUri,routeArgExternalUri)
+//  rspJson.end_session_endpoint   = rspJson.end_session_endpoint.replace(routeArgInternalUri,routeArgExternalUri)
+//  rspJson.revocation_endpoint    = rspJson.revocation_endpoint.replace(routeArgInternalUri,routeArgExternalUri)
+//  rspJson.userinfo_endpoint      = rspJson.userinfo_endpoint.replace(routeArgInternalUri,routeArgExternalUri)
 
   response.setEntity(rspJson)
 
