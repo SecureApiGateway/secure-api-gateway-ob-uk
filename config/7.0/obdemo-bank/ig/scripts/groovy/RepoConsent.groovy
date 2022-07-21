ec3b93df7d2ce13b2c20a090eaa2b92f63319c35
frlaura-jianu <laura.jianu@forgerock.com>
2022-07-21 13:37:37 +0300

    Add international scheduled payment consent PATCH/GET case in the Repo consent script

--- config/7.0/obdemo-bank/ig/scripts/groovy/RepoConsent.groovy
+++ config/7.0/obdemo-bank/ig/scripts/groovy/RepoConsent.groovy
@@ -148,6 +148,16 @@ def convertIDMResponse(intentResponseObject, intentType) {
                     "oauth2ClientName"     : intentResponseObject.apiClient.name
             ]
             break
+        case IntentType.PAYMENT_INTERNATIONAL_SCHEDULED_CONSENT:
+            responseObj = [
+                    "id"                   : intentResponseObject._id,
+                    "data"                 : intentResponseObject.Data,
+                    "resourceOwnerUsername": intentResponseObject.user ? intentResponseObject.user._id : null,
+                    "oauth2ClientId"       : intentResponseObject.apiClient.oauth2ClientId,
+                    "oauth2ClientName"     : intentResponseObject.apiClient.name
+            ]
+            break
+
     }
 
     return responseObj;
