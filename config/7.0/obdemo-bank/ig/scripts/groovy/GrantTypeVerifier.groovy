1184ace4d6d50bf104ace4a13c1a055b3ed87512
frlaura-jianu <laura.jianu@forgerock.com>
2022-09-08 15:05:35 +0300

    Update 337

--- config/7.0/obdemo-bank/ig/scripts/groovy/GrantTypeVerifier.groovy
+++ config/7.0/obdemo-bank/ig/scripts/groovy/GrantTypeVerifier.groovy
@@ -3,4 +3,15 @@ logger.debug(SCRIPT_NAME + "Running...")
 
 logger.debug(SCRIPT_NAME + "Access token info: " + contexts.oauth2.accessToken.info)
 logger.debug(SCRIPT_NAME + "Access token info: " + contexts.oauth2.accessToken.info.grant_type)
-next.handle(context,request)
+def tokenGrantType = contexts.oauth2.accessToken.info.grant_type
+if (tokenGrantType == allowedGrantType){
+    next.handle(context,request)
+}
+else {
+    def response = new Response(httpCode)
+    def message = "invalid_grant_type"
+    response.headers['Content-Type'] = "application/json"
+    response.entity = "{ \"error\":\"" + message + "\"}"
+    return response;
+}
+
