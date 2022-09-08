200680d9455fd489922beb709e4b0846a1c9005f
frlaura-jianu <laura.jianu@forgerock.com>
2022-09-08 11:23:26 +0300

    Update 337

--- config/7.0/obdemo-bank/ig/scripts/groovy/GrantTypeVerifier.groovy
+++ config/7.0/obdemo-bank/ig/scripts/groovy/GrantTypeVerifier.groovy
@@ -2,3 +2,5 @@ SCRIPT_NAME = "[GrantTypeVerifier] - "
 logger.debug(SCRIPT_NAME + "Running...")
 
 logger.debug(SCRIPT_NAME + "Access token info: " + contexts.oauth2.accessToken.info)
+logger.debug(SCRIPT_NAME + "Access token info: " + contexts.oauth2.accessToken.info.grant_type)
+next.handle(context,request)
