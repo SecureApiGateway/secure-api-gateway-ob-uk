f4422743cfbcb843dd4cb3200865da54682f7fc7
Dan Badham <dan.badham@forgerock.com>
2022-10-06 13:16:11 +0100

    535: Updating to use new Account Access Intent IDM object structure
    - Updating ProcessAccountConsent.groovy to create IDM objects with the new structure
    - Updating RepoConsent.groovy to query/patch the new structure, note that this script is shared by all intent types so logic has been added to only use the new structure for Account Access Intents. Eventually all intent types will be updated to the new structure
    
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/535

--- config/7.1.0/securebanking/ig/scripts/groovy/ProcessAccountConsent.groovy
+++ config/7.1.0/securebanking/ig/scripts/groovy/ProcessAccountConsent.groovy
@@ -1,3 +1,4 @@
+import org.forgerock.http.protocol.*
 import java.text.SimpleDateFormat
 import java.util.UUID
 
@@ -20,24 +21,22 @@ def method = request.method
 switch(method.toUpperCase()) {
 
     case "POST":
-        accountIntentData = request.entity.getJson()
-        def tz = TimeZone.getTimeZone("UTC");
-        def df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-        df.setTimeZone(tz);
-        def nowAsISO = df.format(new Date());
-
         def consentId = routeArgConsentIdPrefix + UUID.randomUUID().toString()
+        accountIntentData = request.entity.getJson()
+        processAccountAccessRequestData(consentId, accountIntentData)
 
-        accountIntentData._id = consentId
-        accountIntentData.Data.ConsentId = consentId
-        accountIntentData.Data.Status = "AwaitingAuthorisation";
-        accountIntentData.Data.CreationDateTime = nowAsISO
-        accountIntentData.Data.StatusUpdateDateTime = nowAsISO
-        accountIntentData.apiClient = [ "_ref" : "managed/" + routeArgObjApiClient + "/" +  oauth2ClientId ]
+        // NOTE: creating the intent will eventually move to RS, where OBVersion and OBIntentObjectType can be determined more easily
+        def version = getObApiVersion(request)
+        def idmIntent = [
+                _id: consentId,
+                OBVersion: version,
+                OBIntentObjectType: "OBReadConsentResponse1",
+                OBIntentObject: accountIntentData,
+                apiClient: [ "_ref" : "managed/" + routeArgObjApiClient + "/" +  oauth2ClientId ],
+        ]
 
-        logger.debug(SCRIPT_NAME + "final json [" + accountIntentData + "]")
-        request.setEntity(accountIntentData)
-
+        logger.debug(SCRIPT_NAME + "IDM object json [" + idmIntent + "]")
+        request.setEntity(idmIntent)
         request.uri.path = "/openidm/managed/" + routeArgObjAccountAccessConsent
         request.uri.query = "action=create";
         break
@@ -49,19 +48,54 @@ switch(method.toUpperCase()) {
 
     case "GET":
         def consentId = request.uri.path.substring(request.uri.path.lastIndexOf("/") + 1);
+        // Query IDM for a consent with the matching id, only return the OBIntentObject field
         request.uri.path = "/openidm/managed/" + routeArgObjAccountAccessConsent + "/" + consentId
+        request.uri.query = "_fields=OBIntentObject"
         break
 
     default:
-        logger.debug(SCRIPT_NAME + "Method not supported")
+        logger.debug(SCRIPT_NAME + "Method not supported: " + method)
+        return new Response(Status.METHOD_NOT_ALLOWED);
+}
+return next.handle(context, request).then(this.&extractOBIntentObjectFromIdmResponse)
 
+/**
+ * Responses from IDM will always contain the "OBIntentObject" as a top level field (even if we filter).
+ * We want to send only the contents of the OBIntentObject as the response to the client i.e. a valid Open Banking API response
+ */
+private Response extractOBIntentObjectFromIdmResponse(response) {
+    if (response.status.isSuccessful()) {
+        response.entity = response.entity.getJson().get("OBIntentObject")
+    }
+    return response
 }
 
+private void processAccountAccessRequestData(consentId, accountIntentData) {
+    def tz = TimeZone.getTimeZone("UTC");
+    def df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+    df.setTimeZone(tz);
+    def nowAsISO = df.format(new Date())
+    accountIntentData.Data.ConsentId = consentId
+    accountIntentData.Data.Status = "AwaitingAuthorisation";
+    accountIntentData.Data.CreationDateTime = nowAsISO
+    accountIntentData.Data.StatusUpdateDateTime = nowAsISO
+}
 
-next.handle(context, request)
-
-
-
-
-
-
+/**
+ * Extract the Open Banking Api version from the request uri
+ * Example uri: /rs/open-banking/v3.1.10/aisp/account-access-consents
+ */
+private String getObApiVersion(request) {
+    def uri = request.uri.toString()
+    def pathPrefix = "/rs/open-banking/"
+    int prefixEndIndex = uri.indexOf(pathPrefix)
+    if (prefixEndIndex < 0) {
+        throw new IllegalStateException("Failed to determine OB API version from uri: " + uri)
+    }
+    int versionStartIndex = prefixEndIndex + pathPrefix.length()
+    int versionEndIndex = uri.indexOf('/', versionStartIndex)
+    if (versionEndIndex < 0) {
+        throw new IllegalStateException("Failed to determine OB API version from uri: " + uri)
+    }
+    return uri.substring(versionStartIndex, versionEndIndex)
+}
