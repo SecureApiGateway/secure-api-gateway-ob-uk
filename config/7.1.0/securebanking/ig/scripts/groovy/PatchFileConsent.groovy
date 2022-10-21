bfcb44fd3cac749aa35af2bc37057792122f24b0
mariantiris <47982900+mariantiris@users.noreply.github.com>
2022-10-21 16:12:55 +0300

    Splitting consent route in two separate routes. (#197)
    * Splitting consent route in two separate routes.
    
    * Updates in validation and patch

---
+++ config/7.1.0/securebanking/ig/scripts/groovy/PatchFileConsent.groovy
@@ -0,0 +1,65 @@
+import org.forgerock.http.protocol.*
+import org.forgerock.json.jose.*
+import groovy.json.JsonOutput
+
+// Start script processing area
+
+SCRIPT_NAME = "[PatchFileConsent] - "
+logger.debug(SCRIPT_NAME + "Running...")
+
+def splitUri = request.uri.path.split("/")
+def consentId = splitUri[6]
+
+def requestUri = routeArgIdmBaseUri + "/openidm/managed/filePaymentsIntent/" + consentId + "?_fields=_id,FileContent,OBIntentObject/Data/Status";
+logger.debug(SCRIPT_NAME + "The request uri is " + requestUri)
+
+Request patchRequest = new Request();
+patchRequest.setMethod('POST');
+patchRequest.setUri(requestUri + "&_action=patch");
+patchRequest.getHeaders().add("Content-Type", "application/json");
+patchRequest.setEntity(JsonOutput.toJson(buildPatchRequest(request.entity.getString())))
+
+http.send(patchRequest).thenAsync(patchResponse -> {
+    def responseStatus = patchResponse.getStatus();
+    logger.debug(SCRIPT_NAME + "Patch filePaymentsIntent status: " + responseStatus)
+    logger.debug(SCRIPT_NAME + "Patch filePaymentsIntent response entity: " + patchResponse.entity.getJson())
+
+    if (responseStatus != Status.OK) {
+        return newResultPromise(getPatchError())
+    }
+
+    logger.debug(SCRIPT_NAME + "The intent status was changed to AwaitingAuthorisation and FileContent has been added to the intent object.")
+    return newResultPromise(new Response(Status.OK))
+})
+
+// Start method declaration area
+
+def buildPatchRequest(String fileContent) {
+    def body = [];
+
+    body.push([
+            "operation": "replace",
+            "field"    : "FileContent",
+            "value"    : fileContent
+    ]);
+
+    body.push([
+            "operation": "replace",
+            "field"    : "OBIntentObject/Data/Status",
+            "value"    : "AwaitingAuthorisation"
+    ]);
+
+    return body
+}
+
+/**
+ * Builds an error message for IDM patch failure scenario
+ * @return error response
+ */
+def getPatchError() {
+    message = "Consent could not be patched"
+    logger.error(SCRIPT_NAME + message)
+    Response response = new Response(Status.BAD_REQUEST)
+    response.setEntity("{ \"error\":\"" + message + "\"}")
+    return response;
+}
