5e588b293322100b5b9a72fd1f7dda46d2238eb0
Marian Tiris <marian.tiris@itsmartsystems.eu>
2022-10-21 15:51:45 +0300

    Updates in validation and patch

--- config/7.1.0/securebanking/ig/scripts/groovy/ValidateFileConsent.groovy
+++ config/7.1.0/securebanking/ig/scripts/groovy/ValidateFileConsent.groovy
@@ -1,51 +1,76 @@
 import org.forgerock.http.protocol.*
 import org.forgerock.json.jose.*
 
+// Start script processing area
+
 SCRIPT_NAME = "[ValidateFileConsent] - "
 logger.debug(SCRIPT_NAME + "Running...")
-def method = request.method
 
-switch(method.toUpperCase()) {
+def splitUri = request.uri.path.split("/")
+def version = splitUri[3]
+def consentId = splitUri[6]
 
-    case "POST":
-        def splitUri = request.uri.path.split("/")
-        def version = splitUri[3]
-        def currentApi = splitUri[5]
+Promise<Boolean, NeverThrowsException> validationResult = callRsToValidateFile(routeArgRsBaseURI, version, consentId);
 
-        // Create the RS Calculate elements API
-        def requestURI = routeArgRsBaseURI + "/backoffice/" + version + "/" + currentApi + "/file-payment-consent"
-        logger.debug(SCRIPT_NAME + " The updated raw request uri: " + requestURI)
+return validationResult.thenAsync(validfFile -> {
+    if (Boolean.FALSE.equals(validfFile)) {
+        return newResultPromise(getFileValidationErrorResponse())
+    }
 
-        Request RSRequest = new Request()
-        RSRequest.setUri(requestURI)
-        RSRequest.setMethod('POST')
-        RSRequest.setEntity(request.entity)
-        if (request.headers.get("x-fapi-financial-id") != null)
-            RSRequest.putHeaders(request.headers.get("x-fapi-financial-id"))
-        logger.debug(SCRIPT_NAME + "Entity to be send to RS file payment endpoint " + request.entity)
+    return next.handle(context, request)
 
-        return http.send(RSRequest).thenAsync(RSResponse -> {
-            def RSResponseStatus = RSResponse.getStatus();
-            if (RSResponseStatus != Status.OK) {
-                message = "Failed to validate file consent"
-                logger.error(SCRIPT_NAME + message)
-                logger.error(SCRIPT_NAME + RSResponse.getEntity().getJson())
-                def response = new Response(RSResponseStatus)
-                response.status = RSResponseStatus
-                response.entity = RSResponse.entity.getJson()
-                return newResultPromise(response)
-            }
+})
 
-            def RSResponseContent = RSResponse.getEntity();
-            def RSResponseObject = RSResponseContent.getJson();
+// Start method definition area
 
-            logger.debug(SCRIPT_NAME + "The new entity: " + RSResponseObject.toString())
-            request.setEntity(RSResponseContent.getJson());
+/**
+ * Calls RS to validate the request payload which should be either a JSON file, or an XML file
+ *
+ * @param routeArgRsBaseURI - the RS URL
+ * @param version the API version called
+ * @param consentId the consent ID
+ * @return
+ */
+def callRsToValidateFile(String routeArgRsBaseURI, String version, String consentId) {
+    def requestURI = routeArgRsBaseURI + "/backoffice/" + version + "/file-payment-consent/" +
+            consentId + "/file/validate"
+    logger.debug(SCRIPT_NAME + " The updated request uri: " + requestURI)
 
-            return next.handle(context, request)
-        })
-    default:
-        logger.debug(SCRIPT_NAME + "Skipped")
+    Request RSRequest = new Request()
+    RSRequest.setUri(requestURI)
+    RSRequest.setMethod('POST')
+    RSRequest.getHeaders().add("Authorization", request.getHeaders().getFirst("Authorization"));
+    RSRequest.getHeaders().add("Content-Type", request.getHeaders().getFirst("Content-Type"));
+    RSRequest.setEntity(request.entity.getString())
+
+    if (request.headers.get("x-fapi-financial-id") != null) {
+        RSRequest.putHeaders(request.headers.get("x-fapi-financial-id"))
+    }
+
+    logger.debug(SCRIPT_NAME + "Entity to be send to RS file payment endpoint " + request.entity.getString())
+
+    return http.send(RSRequest).thenAsync(RSResponse -> {
+        def RSResponseStatus = RSResponse.getStatus();
+
+        logger.debug(SCRIPT_NAME + "Validation response: " + RSResponseStatus)
+
+        if (RSResponseStatus != Status.OK) {
+            message = "Failed to validate file consent"
+            logger.error(SCRIPT_NAME + message)
+            return newResultPromise(false)
+        }
+        return newResultPromise(true)
+    })
 }
 
-next.handle(context, request)
+/**
+ * Builds the file validation failure error response
+ * @return error response
+ */
+def getFileValidationErrorResponse() {
+    message = "Invalid payment file"
+    logger.error(SCRIPT_NAME + message)
+    Response response = new Response(Status.BAD_REQUEST)
+    response.setEntity("{ \"error\":\"" + message + "\"}")
+    return response;
+}
