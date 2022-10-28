b61bfd8b2ae7edce7c3edd9d969ccfd18258a40e
Dan Badham <dan.badham@forgerock.com>
2022-10-28 09:54:16 +0100

    597: Fixing bug where idm apiClient is being deleted even if the AM delete is rejected
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/597

--- config/7.1.0/securebanking/ig/scripts/groovy/CreateApiClient.groovy
+++ config/7.1.0/securebanking/ig/scripts/groovy/CreateApiClient.groovy
@@ -170,13 +170,20 @@ switch(method.toUpperCase()) {
     break
   case "DELETE":
     return next.handle(context, request).thenOnResult(response -> {
-      // ProcessRegistration filter will have added the client_id param
-      def apiClientId = request.getQueryParams().getFirst("client_id")
-      Request deleteApiClientReq = new Request()
-      deleteApiClientReq.setMethod('DELETE')
-      deleteApiClientReq.setUri(routeArgIdmBaseUri + "/openidm/managed/" + routeArgObjApiClient + "/" + apiClientId)
-      logger.info("Deleting IDM object: " + routeArgObjApiClient + " for client_id: " + apiClientId)
-      return http.send(deleteApiClientReq)
+      if (response.status.isSuccessful()) {
+        // ProcessRegistration filter will have added the client_id param
+        def apiClientId = request.getQueryParams().getFirst("client_id")
+        Request deleteApiClientReq = new Request()
+        deleteApiClientReq.setMethod('DELETE')
+        deleteApiClientReq.setUri(routeArgIdmBaseUri + "/openidm/managed/" + routeArgObjApiClient + "/" + apiClientId)
+        logger.info("Deleting IDM object: " + routeArgObjApiClient + " for client_id: " + apiClientId)
+        return http.send(deleteApiClientReq).thenOnResult(idmResponse -> {
+          if (idmResponse.status.isSuccessful()) {
+            return new Response(Status.NO_CONTENT)
+          }
+          return (errorResponse(Status.BAD_REQUEST, "Failed to delete registration"));
+        })
+      }
     })
   default:
     logger.debug(SCRIPT_NAME + "Method not supported")
