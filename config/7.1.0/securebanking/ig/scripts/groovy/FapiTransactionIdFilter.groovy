d5fa34cec7887da8ec1ef8b27040964a1c95e77e
bohocode <jamie.bowen@forgerock.com>
2022-11-07 10:47:32 +0000

    257: Create FapiTransactionIdFilter and use in test route
    Create a filter as suggested by Guillaume Sauthier in the following
    issue: https://bugster.forgerock.org/jira/browse/OPENIG-6538
    
    The aim is to set the transactionIdContext to be the value of the
    x-fapi-interaction-id. This _should_ ensure that the
    value of the x-fapi-interaction-id assigned by IG is passed to AM & IDM
    as the x-forgerock-transactionid header and will make logs traceable
    across the system.
    
    Issue: https://github.com/securebankingaccesstoolkit/securebankingaccesstoolkit/issues/257

---
+++ config/7.1.0/securebanking/ig/scripts/groovy/FapiTransactionIdFilter.groovy
@@ -0,0 +1,13 @@
+import org.forgerock.services.context.TransactionIdContext
+import org.forgerock.services.TransactionId
+
+SCRIPT_NAME = "[TransactionIdFilter] - "
+logger.debug(SCRIPT_NAME + "Running...")
+
+String interactionId = request.getHeaders().getFirst("x-fapi-interaction-id");
+if (interactionId != null) {
+    logger.debug(SCRIPT_NAME + "Found x-fapi-interaction-id: " + interactionId + " setting as TransactionId")
+    TransactionIdContext newContext = new TransactionIdContext(context, new TransactionId(interactionId));
+    return next.handle(newContext, request);
+}
+return next.handle(context, request);
