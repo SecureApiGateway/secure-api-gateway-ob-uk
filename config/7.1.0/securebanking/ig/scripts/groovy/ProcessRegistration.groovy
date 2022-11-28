17e30850fcaaf6b521a17b5f9422a6bbf3e51f06
Dan Badham <dan.badham@forgerock.com>
2022-11-28 09:02:24 +0000

    690: Fixing reference to tokenEndpointAuthMethodsSupported in log entry
    Issue: https://github.com/SecureApiGateway/SecureApiGateway/issues/690

--- config/7.1.0/securebanking/ig/scripts/groovy/ProcessRegistration.groovy
+++ config/7.1.0/securebanking/ig/scripts/groovy/ProcessRegistration.groovy
@@ -74,7 +74,7 @@ switch(method.toUpperCase()) {
 
         def tokenEndpointAuthMethod = oidcRegistration.getClaim("token_endpoint_auth_method")
         if (!tokenEndpointAuthMethod || !tokenEndpointAuthMethodsSupported.contains(tokenEndpointAuthMethod)) {
-            return errorResponseFactory.invalidClientMetadataErrorResponse("token_endpoint_auth_method claim must be one of: " + supportedTokenEndpointAuthMethods)
+            return errorResponseFactory.invalidClientMetadataErrorResponse("token_endpoint_auth_method claim must be one of: " + tokenEndpointAuthMethodsSupported)
         }
 
         def ssa = oidcRegistration.getClaim("software_statement", String.class);
