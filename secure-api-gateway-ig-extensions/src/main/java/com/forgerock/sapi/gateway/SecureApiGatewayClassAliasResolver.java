d7fb6a120df0e91979038a18bd4e11ddcbd4cfdc
Dan Badham <106093201+dbadham-fr@users.noreply.github.com>
2023-01-03 15:00:43 +0000

    729: FetchApiClientFilter impl which queries IDM for ApiClient data (#261)
    * 729: Adding FetchApiClientFilter
    
    Adding data model classes ApiClient and ApiClientOrganistation.
    
    FetchApiClientFilter queries IDM for ApiClient data based on the client_id found in the OAuth2Context. The filter than adds the ApiClient as a context attribute so that it can be used by other filters.
    
    NOTE: As the filter depends on the OAuth2Context then it must be installed in the chain after a filter which adds this context, typically: OAuth2ResourceServerFilter
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/729

--- secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/SecureApiGatewayClassAliasResolver.java
+++ secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/SecureApiGatewayClassAliasResolver.java
@@ -20,6 +20,7 @@ import java.util.Map;
 
 import org.forgerock.openig.alias.ClassAliasResolver;
 
+import com.forgerock.sapi.gateway.dcr.FetchApiClientFilter;
 import com.forgerock.sapi.gateway.fapi.v1.FAPIAdvancedDCRValidationFilter;
 import com.forgerock.sapi.gateway.jwks.RestJwkSetService;
 import com.forgerock.sapi.gateway.jwks.cache.caffeine.CaffeineCachingJwkSetService;
@@ -35,6 +36,7 @@ public class SecureApiGatewayClassAliasResolver implements ClassAliasResolver {
         ALIASES.put("RestJwkSetService", RestJwkSetService.class);
         ALIASES.put("RsaJwtSignatureValidator", RsaJwtSignatureValidator.class);
         ALIASES.put("TrustedDirectoriesService", TrustedDirectoryService.class);
+        ALIASES.put("FetchApiClientFilter", FetchApiClientFilter.class);
     }
 
     /**
---
+++ secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/dcr/ApiClientOrganisation.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2020-2022 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.forgerock.sapi.gateway.dcr;
+
+import java.util.Objects;
+
+/**
+ * Data object which represents an Organisation to which one or more {@link ApiClient} objects belong
+ */
+public class ApiClientOrganisation {
+
+    /**
+     * Unique identifier of this organisation.
+     */
+    private final String id;
+
+    /**
+     * Organisation name, typically the company name.
+     */
+    private final String name;
+
+    public ApiClientOrganisation(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final ApiClientOrganisation that = (ApiClientOrganisation) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "ApiClientOrganisation{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
