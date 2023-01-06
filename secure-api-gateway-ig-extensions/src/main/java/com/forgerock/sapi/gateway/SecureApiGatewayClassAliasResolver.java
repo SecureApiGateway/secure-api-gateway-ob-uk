0ba39318dbe5f9e431fe7e898d77bc1c9aed8efd
Dan Badham <dan.badham@forgerock.com>
2023-01-06 09:38:13 +0000

    689: Fixing the class name alias, adding DefaultTransportCertValidator
    https://github.com/SecureApiGateway/SecureApiGateway/issues/689

--- secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/SecureApiGatewayClassAliasResolver.java
+++ secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/SecureApiGatewayClassAliasResolver.java
@@ -26,8 +26,8 @@ import com.forgerock.sapi.gateway.jwks.FetchApiClientJwksFilter;
 import com.forgerock.sapi.gateway.jwks.RestJwkSetService;
 import com.forgerock.sapi.gateway.jwks.cache.caffeine.CaffeineCachingJwkSetService;
 import com.forgerock.sapi.gateway.jws.RsaJwtSignatureValidator;
+import com.forgerock.sapi.gateway.mtls.DefaultTransportCertValidator;
 import com.forgerock.sapi.gateway.mtls.TransportCertValidationFilter;
-import com.forgerock.sapi.gateway.mtls.TransportCertValidator;
 import com.forgerock.sapi.gateway.trusteddirectories.FetchTrustedDirectoryFilter;
 import com.forgerock.sapi.gateway.trusteddirectories.TrustedDirectoryService;
 
@@ -44,7 +44,7 @@ public class SecureApiGatewayClassAliasResolver implements ClassAliasResolver {
         ALIASES.put("FetchTrustedDirectoryFilter", FetchTrustedDirectoryFilter.class);
         ALIASES.put("FetchApiClientJwksFilter", FetchApiClientJwksFilter.class);
         ALIASES.put("TransportCertValidationFilter", TransportCertValidationFilter.class);
-        ALIASES.put("TransportCertValidator", TransportCertValidator.class);
+        ALIASES.put("DefaultTransportCertValidator", DefaultTransportCertValidator.class);
     }
 
     /**
