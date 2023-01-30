8481fc187f92f282df9e89bb869743263bf71414
Laura Jianu <108405474+frlaura-jianu@users.noreply.github.com>
2023-01-30 15:55:32 +0200

    736: Allow only Sweeping VRPType (#293)
    * 736: Allow only Sweeping VRPType
    
    Update VRPTypeVerifier.groovy
    
    Update VRPTypeVerifier.groovy
    
    Update VRPTypeVerifier.groovy
    
    Update VRPTypeVerifier.groovy
    
    Update VRPTypeVerifier.groovy
    
    Update VRPTypeVerifier.groovy
    
    Update VRPTypeVerifier.groovy
    
    Update VRPTypeVerifier.groovy
    
    736: Adding a ScriptableFilter to check the VRPType
    
    Update 62-ob-domestic-vrp-consent.json
    
    Update 62-ob-domestic-vrp-consent.json
    
    Update 62-ob-domestic-vrp-consent.json
    
    Update 62-ob-domestic-vrp-consent.json
    
    Update 62-ob-domestic-vrp-consent.json
    
    Update 62-ob-domestic-vrp-consent.json
    
    Update 62-ob-domestic-vrp-consent.json
    
    736: Adding missing ")"
    
    736: Allow only Sweeping VRPType
    
    Issue: https://github.com/secureapigateway/secureapigateway/issues/736
    Description: Added a new filter to the consent route to check if the VRPType is "Sweeping".
    
    * 736: Fixing VRPTypeVerifier script

---
+++ config/7.1.0/securebanking/ig/scripts/groovy/VRPTypeVerifier.groovy
@@ -0,0 +1,26 @@
+import org.forgerock.http.protocol.*
+import com.forgerock.sapi.gateway.rest.HttpHeaderNames
+
+String fapiInteractionId = request.getHeaders().getFirst(HttpHeaderNames.X_FAPI_INTERACTION_ID)
+if (fapiInteractionId == null) {
+    fapiInteractionId = 'No ' + HttpHeaderNames.X_FAPI_INTERACTION_ID + ' header'
+}
+SCRIPT_NAME = '[VRPTypeVerifier] (' + fapiInteractionId + ') - '
+logger.debug(SCRIPT_NAME + 'Running...')
+
+def vrpTypeFromRequest = request.entity.getJson().Data.ControlParameters.VRPType
+logger.debug(SCRIPT_NAME + 'ControlParameters' + request.entity.getJson().Data.ControlParameters)
+logger.debug(SCRIPT_NAME + 'vrpTypeFromRequest: ' + vrpTypeFromRequest)
+
+// Only allow the request to continue if we have a single VRPType and its value is Sweeping
+if (vrpTypeFromRequest.size() == 1 && vrpTypeFromRequest.contains('UK.OBIE.VRPType.Sweeping')) {
+    return next.handle(context, request)
+}
+
+Response response = new Response(Status.BAD_REQUEST)
+String message = 'Invalid VRP type, only Sweeping payments are supported.'
+logger.error(SCRIPT_NAME + message)
+response.headers['Content-Type'] = 'application/json'
+response.entity = "{ \"error\":\"" + message + "\"}"
+return response
+
