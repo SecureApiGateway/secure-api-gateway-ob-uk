77fc90823ce45981e31f2446dfde30af566ff224
Jorge Sanchez Perez <54277573+jorgesanchezperez@users.noreply.github.com>
2023-01-31 10:15:17 +0000

    736: support vrp type sweeping only (#296)
    - Fix VRPTypeVerifier groovy script to process the check only when the method is post
    Issue: https://github.com/secureapigateway/secureapigateway/issues/736

--- config/7.1.0/securebanking/ig/scripts/groovy/VRPTypeVerifier.groovy
+++ config/7.1.0/securebanking/ig/scripts/groovy/VRPTypeVerifier.groovy
@@ -8,19 +8,24 @@ if (fapiInteractionId == null) {
 SCRIPT_NAME = '[VRPTypeVerifier] (' + fapiInteractionId + ') - '
 logger.debug(SCRIPT_NAME + 'Running...')
 
-def vrpTypeFromRequest = request.entity.getJson().Data.ControlParameters.VRPType
-logger.debug(SCRIPT_NAME + 'ControlParameters' + request.entity.getJson().Data.ControlParameters)
-logger.debug(SCRIPT_NAME + 'vrpTypeFromRequest: ' + vrpTypeFromRequest)
+// the entity only exist when the consent is submitted: POST method
+if(request.getMethod() == "POST") {
+    def vrpTypeFromRequest = request.entity.getJson().Data.ControlParameters.VRPType
+    logger.debug(SCRIPT_NAME + 'ControlParameters' + request.entity.getJson().Data.ControlParameters)
+    logger.debug(SCRIPT_NAME + 'vrpTypeFromRequest: ' + vrpTypeFromRequest)
 
-// Only allow the request to continue if we have a single VRPType and its value is Sweeping
-if (vrpTypeFromRequest.size() == 1 && vrpTypeFromRequest.contains('UK.OBIE.VRPType.Sweeping')) {
-    return next.handle(context, request)
+    // Only allow the request to continue if we have a single VRPType and its value is Sweeping
+    if (vrpTypeFromRequest.size() == 1 && vrpTypeFromRequest.contains('UK.OBIE.VRPType.Sweeping')) {
+        return next.handle(context, request)
+    }
+
+    Response response = new Response(Status.BAD_REQUEST)
+    String message = 'Invalid VRP type, only Sweeping payments are supported.'
+    logger.error(SCRIPT_NAME + message)
+    response.headers['Content-Type'] = 'application/json'
+    response.entity = "{ \"error\":\"" + message + "\"}"
+    return response
 }
 
-Response response = new Response(Status.BAD_REQUEST)
-String message = 'Invalid VRP type, only Sweeping payments are supported.'
-logger.error(SCRIPT_NAME + message)
-response.headers['Content-Type'] = 'application/json'
-response.entity = "{ \"error\":\"" + message + "\"}"
-return response
+next.handle(context, request)
 
