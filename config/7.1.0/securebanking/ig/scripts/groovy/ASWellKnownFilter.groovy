03063409f9ce1e5f96838f7b91184a0e05a84375
Dan Badham <dan.badham@forgerock.com>
2023-02-21 17:36:13 +0000

    838: Updating AS .well-known response to use the mtls hostname for endpoints which require OB mtls
    Added mtlsEndpoints configuration to the route, currently the registration_endpoint and token_endpoint are configured for mtls.
    
    Updated to filter to update the .well-known response to replace the IG hostname with the MTLS hostname for the configured endpoints
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/838

--- config/7.1.0/securebanking/ig/scripts/groovy/ASWellKnownFilter.groovy
+++ config/7.1.0/securebanking/ig/scripts/groovy/ASWellKnownFilter.groovy
@@ -9,6 +9,12 @@ next.handle(context, request).thenOnResult(response -> {
         wellKnownData = response.entity.getJson()
         // Configure auth methods supported using filter arg: tokenEndpointAuthMethodsSupported
         wellKnownData["token_endpoint_auth_methods_supported"] = tokenEndpointAuthMethodsSupported
+
+        // Update endpoints defined in mtlsEndpoints arg to use the mtls host
+        mtlsEndpoints.each { endpoint ->
+            wellKnownData[endpoint] = wellKnownData[endpoint].replace(igHost, mtlsHost)
+        }
+
         response.entity.setJson(wellKnownData)
     }
 })
