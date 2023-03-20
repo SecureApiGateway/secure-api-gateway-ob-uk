26fe3b3b07388ca4912cc3e240c4f23fc95a3ba0
Dan Badham <dan.badham@forgerock.com>
2023-03-20 15:13:22 +0000

    865: Cleanup ApiClientRoleCheck script
    https://github.com/SecureApiGateway/SecureApiGateway/issues/865

--- config/7.1.0/securebanking/ig/scripts/groovy/ApiClientRoleCheck.groovy
+++ config/7.1.0/securebanking/ig/scripts/groovy/ApiClientRoleCheck.groovy
@@ -7,31 +7,20 @@ if(fapiInteractionId == null) fapiInteractionId = "No x-fapi-interaction-id";
 SCRIPT_NAME = "[ApiClientRoleCheck] (" + fapiInteractionId + ") - ";
 
 logger.debug(SCRIPT_NAME + "Running...")
-logger.debug(SCRIPT_NAME + "Checking certificate roles for {} request",routeArgRole)
+logger.debug(SCRIPT_NAME + "Checking certificate roles for {} request", routeArgRole)
 
 if (!attributes.apiClient){
   logger.error("FetchApiClientFilter must be run before this script. apiClient needs to exist in the attributes context");
   return new Response(Status.INTERNAL_SERVER_ERROR)
 }
 
-// response object
-response = new Response(Status.OK)
-response.headers['Content-Type'] = "application/json"
-
-def roles = attributes.clientCertificate.roles
-if (!roles) {
-  message = "No roles in apiClient for TPP role check"
-  logger.error(SCRIPT_NAME + message)
-  response.status = Status.BAD_REQUEST
-  response.entity = "{ \"error\":\"" + message + "\"}"
-  return response
-}
-
 if (!attributes.apiClient.getRoles().contains(routeArgRole)) {
-  message = "client is not authorized to perform role " + routeArgRole
-  logger.error(SCRIPT_NAME + message)
-  response.status = Status.FORBIDDEN
-  response.entity = "{ \"error\":\"" + message + "\"}"
+  def errorMessage = "client is not authorized to perform role: " + routeArgRole
+  logger.warn(SCRIPT_NAME + "ApiClient.id=" + attributes.apiClient.getOauth2ClientId() +  errorMessage)
+  
+  def response = new Response(Status.FORBIDDEN)
+  response.entity = json(object(field("error", errorMessage)))
   return response
 }
+
 next.handle(context, request)
