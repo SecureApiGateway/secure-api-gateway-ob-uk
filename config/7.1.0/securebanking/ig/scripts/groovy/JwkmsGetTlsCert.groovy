af656a90e71378d5cb775f68a490de56fd5cbfd9
bohocode <jamie.bowen@forgerock.com>
2023-04-12 12:30:30 +0100

    945: Add route to return key/pem for signing cert
    There is a convenience method to allow clients to obtain the tls key/pem
    pair from the jwks issued by IG. There needs to be one for the sig
    key/pem pair too, then clients can easily obtain the key to use for
    signing
    
    Issue: https://github.com/secureapigateway/secureapigateway/issues/945

--- config/7.1.0/securebanking/ig/scripts/groovy/JwkmsGetTlsCert.groovy
+++ config/7.1.0/securebanking/ig/scripts/groovy/JwkmsGetTlsCert.groovy
@@ -10,16 +10,18 @@ if(fapiInteractionId == null) fapiInteractionId = "No x-fapi-interaction-id";
 SCRIPT_NAME = "[JwkmsGetTlsCert] (" + fapiInteractionId + ") - ";
 logger.debug(SCRIPT_NAME + "Running...")
 
-def getTlsKey(jwks) {
+logger.debug("obtaining certs of type " + keyType)
+
+RSAKey getKeyOfType(jwks, keyType) {
     List<RSAKey> jwkKeys = jwks.getKeys();
 
-    def key = null;
+    RSAKey key = null;
 
     jwkKeys.forEach(k -> {
         def use = k.getKeyUse().identifier();
         logger.debug(SCRIPT_NAME + "Key use " + use);
-        if (use == "tls") {
-            logger.debug(SCRIPT_NAME + "Found TLS key " + k);
+        if (use == keyType) {
+            logger.debug(SCRIPT_NAME + "Found " + keyType + " key " + k);
             key = k;
         }
     });
@@ -28,46 +30,50 @@ def getTlsKey(jwks) {
 }
 
 JWKSet jwks = JWKSet.parse(request.entity.getString());
-
 if (!jwks) {
-    logger.error(SCRIPT_NAME + "Couldn't parse request body as JWK set");
-    return new Response(Status.BAD_REQUEST);
+    logger.error(SCRIPT_NAME + "Couldn't parse request body as JWK set")
+    return new Response(Status.BAD_REQUEST)
 }
 
-RSAKey jwk = getTlsKey(jwks);
+if (!keyType) {
+    logger.error(SCRIPT_NAME + "Script must be passed a keyType argument")
+    return new Response(Status.INTERNAL_SERVER_ERROR)
+}
+
+RSAKey jwk = getKeyOfType(jwks, keyType)
 
 if (!jwk) {
-    logger.error(SCRIPT_NAME + "Couldn't find TLS key in JWK set");
-    return new Response(Status.BAD_REQUEST);
+    logger.error(SCRIPT_NAME + "Couldn't find " + keyType + ' key in JWK set')
+    return new Response(Status.BAD_REQUEST)
 }
 
-PrivateKey privateKey = jwk.toPrivateKey();
+PrivateKey privateKey = jwk.toPrivateKey()
 
 if (!privateKey) {
-    logger.error(SCRIPT_NAME + "Couldn't find private key in tls jwk");
-    return new Response(Status.BAD_REQUEST);
+    logger.error(SCRIPT_NAME + "Couldn't find private key in tls jwk")
+    return new Response(Status.BAD_REQUEST)
 }
 
 List<X509Certificate> certChain = jwk.getParsedX509CertChain();
 
 if (!certChain) {
-    logger.error(SCRIPT_NAME + "Couldn't find cert chain in tls jwk");
-    return new Response(Status.BAD_REQUEST);
+    logger.error(SCRIPT_NAME + "Couldn't find cert chain in tls jwk")
+    return new Response(Status.BAD_REQUEST)
 }
 
 def pemify(obj) {
-    StringWriter writer = new StringWriter();
-    JcaPEMWriter pemWriter = new JcaPEMWriter(writer);
-    pemWriter.writeObject(obj);
-    pemWriter.flush();
-    pemWriter.close();
+    StringWriter writer = new StringWriter()
+    JcaPEMWriter pemWriter = new JcaPEMWriter(writer)
+    pemWriter.writeObject(obj)
+    pemWriter.flush()
+    pemWriter.close()
 
-    return writer.toString();
+    return writer.toString()
 }
 
-def certPem = "";
-certChain.forEach(cert -> { certPem = certPem.concat(pemify(cert))});
-def keyPem  = pemify(privateKey);
+def certPem = ""
+certChain.forEach(cert -> { certPem = certPem.concat(pemify(cert))})
+def keyPem  = pemify(privateKey)
 
 Response response = new Response(Status.OK)
 response.getHeaders().add("Content-Type","text/plain");
