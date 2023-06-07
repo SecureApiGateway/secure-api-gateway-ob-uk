e53e2032c56d7ccf5d89a2ab0546f37fd6615443
jorgesanchezperez <jorge.sanchez-perez@forgerock.com>
2023-06-07 13:51:27 +0100

    1005: test user data - Repo user groovy script modified to support queryFilter to retrieve psu user by username
    Issue: https://github.com/SecureApiGateway/SecureApiGateway/issues/1005

--- config/7.1.0/securebanking/ig/scripts/groovy/RepoUser.groovy
+++ config/7.1.0/securebanking/ig/scripts/groovy/RepoUser.groovy
@@ -14,39 +14,60 @@ userRequest.setMethod('GET');
 response = new Response(Status.OK)
 response.headers['Content-Type'] = "application/json"
 
-def splitUri =  request.uri.path.split("/")
+// split uri by filter request param
+def uriFilter = request.uri.toString().split("\\?")
+// split uri path to validate it
+def splitUriPath =  request.uri.path.split("/")
 
-if (splitUri.length == 0) {
-    message = "Can't parse api client ID from inbound request"
+// validate uri path
+if (splitUriPath.length == 0) {
+    message = "Can't parse URI from inbound request"
     logger.error(SCRIPT_NAME + message)
     response.status = Status.BAD_REQUEST
     response.entity = "{ \"error\":\"" + message + "\"}"
     return response
 }
 
-def userId = splitUri[splitUri.length - 1];
+Boolean queryFilter = false
+// condition IDM request to retrieve the user data by user name or by user ID
+if(uriFilter.length == 2){
+    logger.debug(SCRIPT_NAME + "Looking up API User by filter {}", uriFilter[uriFilter.length -1])
+    userRequest.setUri(routeArgIdmBaseUri + "/openidm/managed/" + routeArgObjUser + "?" + uriFilter[uriFilter.length -1])
+    queryFilter = true
+} else {
+    logger.debug(SCRIPT_NAME + "Looking up API User by user ID {}", splitUriPath[splitUriPath.length - 1])
+    userRequest.setUri(routeArgIdmBaseUri + "/openidm/managed/" + routeArgObjUser + "/" + splitUriPath[splitUriPath.length - 1])
+}
 
-logger.debug(SCRIPT_NAME + "Looking up API User {}",userId)
-
-userRequest.setUri(routeArgIdmBaseUri + "/openidm/managed/" + routeArgObjUser + "/" + userId)
+/*
+ examples to test the filter. It needs to be run from a container:
+ - curl -i -v http://ig:80/repo/users?_queryFilter=userName+eq+%22<Cloud platform user name>%22
+ - curl -i -v http://ig:80/repo/users/<Cloud platform user ID>
+ */
 
 http.send(userRequest).then(userResponse -> {
     userRequest.close()
     logger.debug(SCRIPT_NAME + "Back from IDM")
 
     def userResponseStatus = userResponse.getStatus();
+    logger.debug(SCRIPT_NAME + " status {}", userResponseStatus)
 
     if (userResponseStatus != Status.OK) {
-        message = "Failed to get user details"
-        logger.error(SCRIPT_NAME + message)
-        response.status = userResponseStatus
-        response.entity = "{ \"error\":\"" + message + "\"}"
-        return response
+        return errorResponse("User details not found", userResponseStatus)
     }
 
-    def userResponseContent = userResponse.getEntity();
-    def userResponseObject = userResponseContent.getJson();
-    logger.debug(SCRIPT_NAME + "response JSON " + userResponseObject);
+    def userResponseContent = userResponse.getEntity()
+
+    logger.debug(SCRIPT_NAME + "response JSON {}", userResponseContent.getJson().result)
+
+    // build response Object
+    def userResponseObject = userResponseContent.getJson()
+    if(queryFilter){
+        if(userResponseContent.getJson().result.isEmpty()){
+            return errorResponse("User details not found", Status.NOT_FOUND)
+        }
+        userResponseObject = userResponseContent.getJson().result[0]
+    }
 
     def responseObj = [
             "id": userResponseObject._id,
@@ -57,10 +78,17 @@ http.send(userRequest).then(userResponse -> {
             "accountStatus": userResponseObject.accountStatus
     ]
 
-    def responseJson = JsonOutput.toJson(responseObj);
+    def responseJson = JsonOutput.toJson(responseObj)
     logger.debug(SCRIPT_NAME + "Final JSON " + responseJson)
 
     response.entity = responseJson;
     return response
 
 }).then(response -> { return response })
+
+def errorResponse(String message, userResponseStatus) {
+    logger.error(SCRIPT_NAME + message)
+    response.status = userResponseStatus
+    response.entity = "{ \"error\":\"" + message + "\"}"
+    return response
+}
