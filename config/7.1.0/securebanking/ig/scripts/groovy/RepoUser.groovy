b5d353b2d194b3c7f3763607a5d27ef6610e55cc
Jorge Sanchez Perez <54277573+jorgesanchezperez@users.noreply.github.com>
2023-06-08 12:48:46 +0100

    1005: test user data (#389)
    * 1005: test user data
    - Repo user groovy script modified to support queryFilter to retrieve psu user by username
    
    Issue: https://github.com/SecureApiGateway/SecureApiGateway/issues/1005
    
    * refactored to use the MutableUri util class
    
    * Fix uri query and path validations
    
    * request.uri is a type of MutableUri

--- config/7.1.0/securebanking/ig/scripts/groovy/RepoUser.groovy
+++ config/7.1.0/securebanking/ig/scripts/groovy/RepoUser.groovy
@@ -1,6 +1,5 @@
 import groovy.json.JsonOutput
 
-
 def fapiInteractionId = request.getHeaders().getFirst("x-fapi-interaction-id");
 if(fapiInteractionId == null) fapiInteractionId = "No x-fapi-interaction-id";
 SCRIPT_NAME = "[RepoUser] (" + fapiInteractionId + ") - ";
@@ -14,39 +13,56 @@ userRequest.setMethod('GET');
 response = new Response(Status.OK)
 response.headers['Content-Type'] = "application/json"
 
-def splitUri =  request.uri.path.split("/")
-
-if (splitUri.length == 0) {
-    message = "Can't parse api client ID from inbound request"
-    logger.error(SCRIPT_NAME + message)
-    response.status = Status.BAD_REQUEST
-    response.entity = "{ \"error\":\"" + message + "\"}"
-    return response
+// get raw uri query filter, encoded to avoid illegal character in query
+def uriQuery = request.uri.getRawQuery()
+// get uri path elements
+def uriPathElements =  request.uri.getPathElements()
+logger.debug(SCRIPT_NAME + "uriFilter: {}, splitUriPath: {}", uriQuery, uriPathElements)
+// validate uri path
+if (uriPathElements.isEmpty()) {
+    errorResponse("Not elements found in the uri path, uri path elements is empty from inbound request", Status.BAD_REQUEST)
 }
 
-def userId = splitUri[splitUri.length - 1];
+Boolean queryFilter = false
+// condition IDM request to retrieve the user data by user name or by user ID
+if(Objects.nonNull(uriQuery)){
+    logger.debug(SCRIPT_NAME + "Looking up API User by filter {}", uriQuery)
+    userRequest.setUri(routeArgIdmBaseUri + "/openidm/managed/" + routeArgObjUser + "?" + uriQuery)
+    queryFilter = true
+} else {
+    logger.debug(SCRIPT_NAME + "Looking up API User by user ID {}", uriPathElements[uriPathElements.size() - 1])
+    userRequest.setUri(routeArgIdmBaseUri + "/openidm/managed/" + routeArgObjUser + "/" + uriPathElements[uriPathElements.size() - 1])
+}
 
-logger.debug(SCRIPT_NAME + "Looking up API User {}",userId)
-
-userRequest.setUri(routeArgIdmBaseUri + "/openidm/managed/" + routeArgObjUser + "/" + userId)
+/*
+ examples to test the filter. It needs to be run from a container:
+ - curl -i -v http://ig:80/repo/users?_queryFilter=userName+eq+%22<Cloud platform user name>%22
+ - curl -i -v http://ig:80/repo/users/<Cloud platform user ID>
+ */
 
 http.send(userRequest).then(userResponse -> {
     userRequest.close()
     logger.debug(SCRIPT_NAME + "Back from IDM")
 
     def userResponseStatus = userResponse.getStatus();
+    logger.debug(SCRIPT_NAME + " status {}", userResponseStatus)
 
     if (userResponseStatus != Status.OK) {
-        message = "Failed to get user details"
-        logger.error(SCRIPT_NAME + message)
-        response.status = userResponseStatus
-        response.entity = "{ \"error\":\"" + message + "\"}"
-        return response
+        return errorResponse("User details not found", userResponseStatus)
     }
 
-    def userResponseContent = userResponse.getEntity();
-    def userResponseObject = userResponseContent.getJson();
-    logger.debug(SCRIPT_NAME + "response JSON " + userResponseObject);
+    def userResponseContent = userResponse.getEntity()
+
+    logger.debug(SCRIPT_NAME + "response JSON {}", userResponseContent.getJson().result)
+
+    // build response Object
+    def userResponseObject = userResponseContent.getJson()
+    if(queryFilter){
+        if(userResponseContent.getJson().result.isEmpty()){
+            return errorResponse("User details not found", Status.NOT_FOUND)
+        }
+        userResponseObject = userResponseContent.getJson().result[0]
+    }
 
     def responseObj = [
             "id": userResponseObject._id,
@@ -57,10 +73,17 @@ http.send(userRequest).then(userResponse -> {
             "accountStatus": userResponseObject.accountStatus
     ]
 
-    def responseJson = JsonOutput.toJson(responseObj);
+    def responseJson = JsonOutput.toJson(responseObj)
     logger.debug(SCRIPT_NAME + "Final JSON " + responseJson)
 
     response.entity = responseJson;
     return response
 
 }).then(response -> { return response })
+
+def errorResponse(String message, userResponseStatus) {
+    logger.error(SCRIPT_NAME + message)
+    response.status = userResponseStatus
+    response.entity = "{ \"error\":\"" + message + "\"}"
+    return response
+}
