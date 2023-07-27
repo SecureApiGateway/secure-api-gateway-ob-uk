7aa31c61007c4dccb6426f1cefa02ae912cfcdea
Dan Badham <106093201+dbadham-fr@users.noreply.github.com>
2023-07-27 16:23:15 +0100

    1052: Validating Funds Confirmation Request ConsentId is valid for the access_token (#413)
    * 1052: Applying ConsentIdValidator to VRP funds confirmation route
    
    ConsentIdValidator ensures that VRP funds confirmation POST request entities have a consentId matching the consentId in the access_token.
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1052
    
    * 1052: Renaming ConsentIdValidator => RequestEntityConsentIdValidator
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1052
    
    * 1052: Adding request URI consentId validation for Funds Confirmation GET requests
    
    For non-VRP payments, Funds Confirmation requests are HTTP GETs. Add validation to check that the request URI consentId path param matches the access_token consentId
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1052

--- config/7.1.0/securebanking/ig/scripts/groovy/RequestPathConsentIdValidator.groovy
+++ config/7.1.0/securebanking/ig/scripts/groovy/RequestPathConsentIdValidator.groovy
@@ -2,13 +2,13 @@ import org.forgerock.http.protocol.*
 import com.forgerock.sapi.gateway.rest.HttpHeaderNames
 
 /**
- * This script is comparing the consentId from the request payload versus the consentId from the provided access token.
+ * This script is comparing the consentId from the request uri path versus the consentId from the provided access token.
  * By doing this comparison, we prevent resources being submitted with a token obtained for another consent.
  */
 
 String fapiInteractionId = request.getHeaders().getFirst(HttpHeaderNames.X_FAPI_INTERACTION_ID);
 if (fapiInteractionId == null) { fapiInteractionId = 'No ' + HttpHeaderNames.X_FAPI_INTERACTION_ID + ' header'}
-SCRIPT_NAME = '[ConsentIdValidator] (' + fapiInteractionId + ') - '
+SCRIPT_NAME = '[RequestPathConsentIdValidator] (' + fapiInteractionId + ') - '
 logger.debug(SCRIPT_NAME + 'Running...')
 
 // Get the intent id from the access token
@@ -18,8 +18,8 @@ if (!accessTokenIntentId) {
     throw new IllegalStateException("openbanking_intent_id claim is missing from the attributes context");
 }
 
-// Get the intent Id from the request body
-def requestIntentId = request.entity.getJson().Data.ConsentId
+// Get the intent Id from the uri
+def requestIntentId = request.uri.pathElements[routeArgConsentIdPathElementIndex]
 
 logger.debug(SCRIPT_NAME + 'Comparing token intent id {} with request intent id {}', accessTokenIntentId, requestIntentId)
 
