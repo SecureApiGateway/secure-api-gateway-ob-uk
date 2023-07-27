d536a3de18c3313001abc9fe51fbf2009f59bde0
Dan Badham <dan.badham@forgerock.com>
2023-07-27 13:08:31 +0100

    1081: Removing filter scripts for OB Consent Biz Logic
    Removing filters no longer references in any routes.
    
    Removing ConsumeThePaymentIntent filter from all payment routes, this filter has been failing and logging errors (but not causing the HTTP Request to fail). Consuming payment consents is now implemented in RS.
    
    Removing RepoConsent filter from internal-repo route as the IDM Consent Repo is no longer used.
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1081

--- config/7.1.0/securebanking/ig/scripts/groovy/TranslatePaymentResource.groovy
+++
@@ -1,8 +0,0 @@
-def fapiInteractionId = request.getHeaders().getFirst("x-fapi-interaction-id");
-if(fapiInteractionId == null) fapiInteractionId = "No x-fapi-interaction-id";
-SCRIPT_NAME = "[TranslatePaymentResource] (" + fapiInteractionId + ") - ";
-logger.debug(SCRIPT_NAME + "Running...")
-
-request.headers.add(routeArgAccountIdHeader, attributes.get("accountId"))
-
-next.handle(context, request)
