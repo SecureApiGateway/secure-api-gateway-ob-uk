SecureApiGateway/secure-api-gateway-ob-uk

commit c7c2011be829b31498f34dd28c3dbd47734955fd

961: Support OAuth 2.0 Pushed Authorization Requests (PAR) (#467)
* 961: Refactoring FapiAuthorizeRequestValidationFilter to enable it to be reused for /par request validation

BaseFapiAuthorizeRequestValidationFilter handles the core logic of validating an authorize request.

FapiAuthorizeRequestValidationFilter handles /authorize specific logic:
- handling Query param based parameters
- skipping validation if this is an /authorize request for a previously submitted /par request i.e. if the request_uri is present

FapiParRequestValidationFilter handles /par specific logic, which is to work with Form based parameters.

Refactored AuthorizeResponseFetchApiClientFilter to work with /par requests by allowing a Function to be plugged in which retrieves the client_id.

Created ParResponseFetchApiClientFilterHeaplet which an be used to create ParResponseFetchApiClientFilters. This filter is an alias for AuthorizeResponseFetchApiClientFilter that has been configured to retrieve the client_id from the Request's Form.

Updated /par IG route to use the PAR specific filters.

https://github.com/SecureApiGateway/SecureApiGateway/issues/961

* 961: Adding tests ParResponseFetchApiClientFilter

Extracting BaseAuthorizeResponseFetchApiClientFilterTest which contains common tests for /authorize and /par endpoints for fetching the ApiClient.

https://github.com/SecureApiGateway/SecureApiGateway/issues/961

---------

Co-authored-by: bohocode <jamie.bowen@forgerock.com>

secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/SecureApiGatewayClassAliasResolver.java
@@ -25,10 +25,12 @@ import com.forgerock.sapi.gateway.common.exception.SapiLogAttachedExceptionFilte
 import com.forgerock.sapi.gateway.consent.ConsentRequestAccessAuthorisationFilter;
 import com.forgerock.sapi.gateway.dcr.idm.AuthorizeResponseFetchApiClientFilter;
 import com.forgerock.sapi.gateway.dcr.idm.FetchApiClientFilter;
+import com.forgerock.sapi.gateway.dcr.idm.ParResponseFetchApiClientFilterHeaplet;
 import com.forgerock.sapi.gateway.dcr.request.RegistrationRequestEntityValidatorFilter;
 import com.forgerock.sapi.gateway.dcr.sigvalidation.RegistrationRequestJwtSignatureValidationFilter;
 import com.forgerock.sapi.gateway.fapi.v1.FAPIAdvancedDCRValidationFilter;
-import com.forgerock.sapi.gateway.fapi.v1.FapiAuthorizeRequestValidationFilter;
+import com.forgerock.sapi.gateway.fapi.v1.authorize.FapiAuthorizeRequestValidationFilter;
+import com.forgerock.sapi.gateway.fapi.v1.authorize.FapiParRequestValidationFilter;
 import com.forgerock.sapi.gateway.jwks.FetchApiClientJwksFilter;
 import com.forgerock.sapi.gateway.jwks.RestJwkSetService;
 import com.forgerock.sapi.gateway.jwks.cache.caffeine.CaffeineCachingJwkSetService;
@@ -73,6 +75,8 @@ public class SecureApiGatewayClassAliasResolver implements ClassAliasResolver {
         ALIASES.put("AuthoriseResponseFetchApiClientFilter", AuthorizeResponseFetchApiClientFilter.class);
         ALIASES.put("TokenEndpointMetricsContextSupplier", TokenEndpointMetricsContextSupplier.class);
         ALIASES.put("FapiAuthorizeRequestValidationFilter", FapiAuthorizeRequestValidationFilter.class);
+        ALIASES.put("FapiParRequestValidationFilter", FapiParRequestValidationFilter.class);
+        ALIASES.put("ParResponseFetchApiClientFilter", ParResponseFetchApiClientFilterHeaplet.class);
     }
 
     /**

secure-api-gateway-ig-extensions/src/test/java/com/forgerock/sapi/gateway/dcr/idm/AuthorizeResponseFetchApiClientFilterTest.java
@@ -15,91 +15,42 @@
  */
 package com.forgerock.sapi.gateway.dcr.idm;
 
-import static com.forgerock.sapi.gateway.dcr.idm.FetchApiClientFilterTest.createApiClientService;
 import static com.forgerock.sapi.gateway.dcr.idm.IdmApiClientDecoderTest.createIdmApiClientDataAllFields;
-import static com.forgerock.sapi.gateway.dcr.idm.IdmApiClientDecoderTest.createIdmApiClientDataRequiredFieldsOnly;
-import static com.forgerock.sapi.gateway.dcr.idm.IdmApiClientDecoderTest.verifyIdmClientDataMatchesApiClientObject;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.forgerock.json.JsonValue.field;
 import static org.forgerock.json.JsonValue.json;
 import static org.forgerock.json.JsonValue.object;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.net.URISyntaxException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.function.BiConsumer;
+import java.util.function.Function;
 
-import org.forgerock.http.Client;
 import org.forgerock.http.Handler;
 import org.forgerock.http.protocol.Request;
 import org.forgerock.http.protocol.Response;
 import org.forgerock.http.protocol.Status;
 import org.forgerock.json.JsonValue;
 import org.forgerock.json.JsonValueException;
-import org.forgerock.openig.handler.Handlers;
 import org.forgerock.openig.heap.HeapException;
 import org.forgerock.openig.heap.HeapImpl;
 import org.forgerock.openig.heap.Name;
-import org.forgerock.services.context.AttributesContext;
-import org.forgerock.services.context.RootContext;
 import org.forgerock.util.promise.NeverThrowsException;
 import org.forgerock.util.promise.Promise;
 import org.forgerock.util.promise.Promises;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import com.forgerock.sapi.gateway.dcr.idm.AuthorizeResponseFetchApiClientFilter.Heaplet;
 import com.forgerock.sapi.gateway.dcr.idm.IdmApiClientServiceTest.MockApiClientTestDataIdmHandler;
-import com.forgerock.sapi.gateway.dcr.models.ApiClient;
-import com.forgerock.sapi.gateway.util.TestHandlers.FixedResponseHandler;
-import com.forgerock.sapi.gateway.util.TestHandlers.TestSuccessResponseHandler;
 
-class AuthorizeResponseFetchApiClientFilterTest {
+class AuthorizeResponseFetchApiClientFilterTest extends BaseAuthorizeResponseFetchApiClientFilterTest {
 
-    static final String idmBaseUri = "http://localhost/openidm/managed/";
-    static final String clientId = "9999";
-
-
-    @Test
-    void fetchApiClientForSuccessResponse() throws Exception {
-        final JsonValue idmClientData = createIdmApiClientDataRequiredFieldsOnly(clientId);
-        final MockApiClientTestDataIdmHandler idmResponseHandler = new MockApiClientTestDataIdmHandler(idmBaseUri, clientId, idmClientData);
-        final AuthorizeResponseFetchApiClientFilter filter = new AuthorizeResponseFetchApiClientFilter(createApiClientService(new Client(idmResponseHandler), idmBaseUri));
-        callFilterValidateSuccessBehaviour(idmClientData, filter);
+    @Override
+    protected Function<Request, Promise<String, NeverThrowsException>> createClientIdRetriever() {
+        return AuthorizeResponseFetchApiClientFilter.queryParamClientIdRetriever();
     }
 
-    private static void callFilterValidateSuccessBehaviour(JsonValue idmClientData, AuthorizeResponseFetchApiClientFilter filter) throws Exception {
-
-        final BiConsumer<Response, AttributesContext> successBehaviourValidator = (response, ctxt) -> {
-            // Verify we hit the end of the chain and got the NO_CONTENT response
-            assertEquals(Status.NO_CONTENT, response.getStatus());
-
-            // Verify that the context was updated with the apiClient data
-            final ApiClient apiClient = FetchApiClientFilter.getApiClientFromContext(ctxt);
-            assertNotNull(apiClient, "apiClient was not found in context");
-            verifyIdmClientDataMatchesApiClientObject(idmClientData, apiClient);
-        };
-        callFilter(filter, successBehaviourValidator);
-    }
-
-    private static void callFilter(AuthorizeResponseFetchApiClientFilter filter, BiConsumer<Response, AttributesContext> responseAndContextValidator) throws Exception {
-        final AttributesContext attributesContext = createContext();
-
-        // This is the next handler called after the AuthoriseResponseFetchApiClientFilter
-        final Handler endOfFilterChainHandler = Handlers.NO_CONTENT;
-        final Request request = createRequest();
-        final Promise<Response, NeverThrowsException> responsePromise = filter.filter(attributesContext, request, endOfFilterChainHandler);
-
-        final Response response = responsePromise.getOrThrow(1L, TimeUnit.SECONDS);
-
-        // Do the validation
-        responseAndContextValidator.accept(response, attributesContext);
-    }
-
-    private static Request createRequest() {
+    @Override
+    protected Request createRequest() {
         final Request request = new Request();
         try {
             request.setUri("/authorize?client_id=" + clientId);
@@ -109,57 +60,11 @@ class AuthorizeResponseFetchApiClientFilterTest {
         return request;
     }
 
-    private static AttributesContext createContext() {
-        return new AttributesContext(new RootContext("root"));
-    }
-
-    @Test
-    void doesNotFetchApiClientForErrorResponses() throws InterruptedException, TimeoutException {
-        final JsonValue idmClientData = createIdmApiClientDataRequiredFieldsOnly(clientId);
-        final MockApiClientTestDataIdmHandler idmResponseHandler = new MockApiClientTestDataIdmHandler(idmBaseUri, clientId, idmClientData);
-        final AuthorizeResponseFetchApiClientFilter filter = new AuthorizeResponseFetchApiClientFilter(createApiClientService(new Client(idmResponseHandler), idmBaseUri));
-        final AttributesContext context = createContext();
-
-        final Promise<Response, NeverThrowsException> responsePromise = filter.filter(context, createRequest(), new FixedResponseHandler(new Response(Status.BAD_GATEWAY)));
-        final Response response = responsePromise.getOrThrow(1, TimeUnit.SECONDS);
-
-        assertThat(response.getStatus()).isEqualTo(Status.BAD_GATEWAY);
-        assertThat(FetchApiClientFilter.getApiClientFromContext(context)).isNull();
-    }
-
-    @Test
-    void returnsErrorResponseWhenClientIdParamNotFound() throws Exception {
-        final JsonValue idmClientData = createIdmApiClientDataRequiredFieldsOnly(clientId);
-        final MockApiClientTestDataIdmHandler idmResponseHandler = new MockApiClientTestDataIdmHandler(idmBaseUri, clientId, idmClientData);
-        final AuthorizeResponseFetchApiClientFilter filter = new AuthorizeResponseFetchApiClientFilter(createApiClientService(new Client(idmResponseHandler), idmBaseUri));
-        final AttributesContext context = createContext();
-
-        final Request request = new Request();
-        request.setUri("/authorize");
-        final Promise<Response, NeverThrowsException> responsePromise = filter.filter(context, request, new TestSuccessResponseHandler());
-        final Response response = responsePromise.getOrThrow(1, TimeUnit.SECONDS);
-
-        assertThat(response.getStatus()).isEqualTo(Status.INTERNAL_SERVER_ERROR);
-        assertThat(FetchApiClientFilter.getApiClientFromContext(context)).isNull();
-    }
-
-    @Test
-    void returnsErrorResponseWhenApiClientServiceReturnsException() throws Exception {
-        final AuthorizeResponseFetchApiClientFilter filter = new AuthorizeResponseFetchApiClientFilter(createApiClientService(new Client(Handlers.INTERNAL_SERVER_ERROR), idmBaseUri));
-        final AttributesContext context = createContext();
-
-        final Promise<Response, NeverThrowsException> responsePromise = filter.filter(context, createRequest(), new TestSuccessResponseHandler());
-        final Response response = responsePromise.getOrThrow(1, TimeUnit.SECONDS);
-
-        assertThat(response.getStatus()).isEqualTo(Status.INTERNAL_SERVER_ERROR);
-        assertThat(FetchApiClientFilter.getApiClientFromContext(context)).isNull();
-    }
-
     @Nested
     public class HeapletTests {
         @Test
         void failsToConstructIfClientHandlerIsMissing() {
-            final HeapException heapException = assertThrows(HeapException.class, () -> new Heaplet().create(Name.of("test"),
+            final HeapException heapException = assertThrows(HeapException.class, () -> new AuthoriseResponseFetchApiClientFilterHeaplet().create(Name.of("test"),
                     json(object()), new HeapImpl(Name.of("heap"))), "Invalid object declaration");
             assertEquals(heapException.getCause().getMessage(), "/clientHandler: Expecting a value");
         }
@@ -170,7 +75,7 @@ class AuthorizeResponseFetchApiClientFilterTest {
             final HeapImpl heap = new HeapImpl(Name.of("heap"));
             heap.put("idmClientHandler", idmClientHandler);
 
-            assertThrows(JsonValueException.class, () -> new Heaplet().create(Name.of("test"),
+            assertThrows(JsonValueException.class, () -> new AuthoriseResponseFetchApiClientFilterHeaplet().create(Name.of("test"),
                     json(object(field("clientHandler", "idmClientHandler"))), heap), "/idmGetApiClientBaseUri: Expecting a value");
         }
 
@@ -184,7 +89,7 @@ class AuthorizeResponseFetchApiClientFilterTest {
 
             final JsonValue config = json(object(field("clientHandler", "idmClientHandler"),
                     field("idmGetApiClientBaseUri", idmBaseUri)));
-            final AuthorizeResponseFetchApiClientFilter filter = (AuthorizeResponseFetchApiClientFilter) new Heaplet().create(Name.of("test"), config, heap);
+            final AuthorizeResponseFetchApiClientFilter filter = (AuthorizeResponseFetchApiClientFilter) new AuthoriseResponseFetchApiClientFilterHeaplet().create(Name.of("test"), config, heap);
 
             // Test the filter created by the Heaplet
             callFilterValidateSuccessBehaviour(idmApiClientData, filter);

secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/dcr/idm/AuthoriseResponseFetchApiClientFilterHeaplet.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright © 2020-2022 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.forgerock.sapi.gateway.dcr.idm;
+
+import org.forgerock.openig.heap.HeapException;
+
+import com.forgerock.sapi.gateway.dcr.idm.FetchApiClientFilter.BaseFetchApiClientHeaplet;
+
+/**
+ * Responsible for creating the {@link AuthorizeResponseFetchApiClientFilter}
+ * <p>
+ * Mandatory config:
+ * - idmGetApiClientBaseUri: the base uri used to build the IDM query to get the apiClient, the client_id is expected
+ * to be appended to this uri (and some query params).
+ * - clientHandler: the clientHandler to use to call out to IDM (must be configured with the credentials required to
+ * query IDM)
+ * <p>
+ * Example config:
+ * <pre>{@code
+ * {
+ *   "comment": "Add ApiClient data to the context attributes for the AS /authorize route",
+ *   "name": "AuthoriseResponseFetchApiClientFilter",
+ *   "type": "AuthoriseResponseFetchApiClientFilter",
+ *   "config": {
+ *     "idmGetApiClientBaseUri": "https://&{identity.platform.fqdn}/openidm/managed/apiClient",
+ *     "clientHandler": "IDMClientHandler"
+ *   }
+ * }
+ * }</pre>
+ */
+public class AuthoriseResponseFetchApiClientFilterHeaplet extends BaseFetchApiClientHeaplet {
+
+    @Override
+    public Object create() throws HeapException {
+        return new AuthorizeResponseFetchApiClientFilter(createApiClientService(), AuthorizeResponseFetchApiClientFilter.queryParamClientIdRetriever());
+    }
+}

secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/dcr/idm/AuthorizeResponseFetchApiClientFilter.java
@@ -19,13 +19,13 @@ import static org.forgerock.json.JsonValue.field;
 import static org.forgerock.json.JsonValue.json;
 
 import java.util.List;
+import java.util.function.Function;
 
 import org.forgerock.http.Filter;
 import org.forgerock.http.Handler;
 import org.forgerock.http.protocol.Request;
 import org.forgerock.http.protocol.Response;
 import org.forgerock.http.protocol.Status;
-import org.forgerock.openig.heap.HeapException;
 import org.forgerock.services.context.Context;
 import org.forgerock.util.Reject;
 import org.forgerock.util.promise.NeverThrowsException;
@@ -36,7 +36,6 @@ import org.slf4j.LoggerFactory;
 
 import com.forgerock.sapi.gateway.dcr.idm.ApiClientService.ApiClientServiceException;
 import com.forgerock.sapi.gateway.dcr.idm.ApiClientService.ApiClientServiceException.ErrorCode;
-import com.forgerock.sapi.gateway.dcr.idm.FetchApiClientFilter.BaseFetchApiClientHeaplet;
 import com.forgerock.sapi.gateway.dcr.models.ApiClient;
 
 /**
@@ -51,44 +50,49 @@ import com.forgerock.sapi.gateway.dcr.models.ApiClient;
  */
 public class AuthorizeResponseFetchApiClientFilter implements Filter {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorizeResponseFetchApiClientFilter.class);
 
     /**
      * Service which can retrieve ApiClient data
      */
     private final ApiClientService apiClientService;
 
-    public AuthorizeResponseFetchApiClientFilter(ApiClientService apiClientService) {
+    /**
+     * Function that can retrieve the clientId of the ApiClient from the Request
+     */
+    private final Function<Request, Promise<String, NeverThrowsException>> clientIdRetriever;
+
+    public AuthorizeResponseFetchApiClientFilter(ApiClientService apiClientService,
+                                                 Function<Request, Promise<String, NeverThrowsException>> clientIdRetriever) {
         this.apiClientService = Reject.checkNotNull(apiClientService, "apiClientService must be provided");
+        this.clientIdRetriever = Reject.checkNotNull(clientIdRetriever, "clientIdRetriever must be provided");
     }
 
     @Override
     public Promise<Response, NeverThrowsException> filter(Context context, Request request, Handler next) {
-        return next.handle(context, request).thenAsync(response -> {
-            if (response.getStatus().isServerError() || response.getStatus().isClientError()) {
-                return Promises.newResultPromise(response);
-            } else {
-                final List<String> clientIdParams = request.getQueryParams().get("client_id");
-                final String clientId;
-                if (clientIdParams != null && clientIdParams.size() > 0) {
-                    clientId =  clientIdParams.get(0);
-                } else {
-                    logger.error("Authorize request missing mandatory client_id param");
-                    return Promises.newResultPromise(new Response(Status.INTERNAL_SERVER_ERROR));
-                }
-                return apiClientService.getApiClient(clientId)
-                                       .thenOnResult(FetchApiClientFilter.createAddApiClientToContextResultHandler(context, logger))
-                                       .then(apiClient -> response, // return the original response from the upstream
-                                             this::handleApiClientServiceException, this::handleUnexpectedException);
-
+        return clientIdRetriever.apply(request).thenAsync(clientId -> {
+            if (clientId == null) {
+                LOGGER.error("Authorize request missing mandatory client_id param");
+                return Promises.newResultPromise(new Response(Status.INTERNAL_SERVER_ERROR));
             }
+            return next.handle(context, request).thenAsync(response -> {
+                if (response.getStatus().isServerError() || response.getStatus().isClientError()) {
+                    return Promises.newResultPromise(response);
+                } else {
+                    return apiClientService.getApiClient(clientId)
+                            .thenOnResult(FetchApiClientFilter.createAddApiClientToContextResultHandler(context, LOGGER))
+                            .then(apiClient -> response, // return the original response from the upstream
+                                    this::handleApiClientServiceException, this::handleUnexpectedException);
+
+                }
+            });
         });
     }
 
     private Response handleApiClientServiceException(ApiClientServiceException ex) {
         // Handles the case where the ApiClient has been deleted from the data store
         if (ex.getErrorCode() == ErrorCode.DELETED || ex.getErrorCode() == ErrorCode.NOT_FOUND) {
-            logger.warn("Failed to get ApiClient due to: {}", ex.getErrorCode(), ex);
+            LOGGER.warn("Failed to get ApiClient due to: {}", ex.getErrorCode(), ex);
             return new Response(Status.UNAUTHORIZED).setEntity(json(field("error", "client registration is invalid")));
         } else {
             return handleUnexpectedException(ex);
@@ -96,35 +100,38 @@ public class AuthorizeResponseFetchApiClientFilter implements Filter {
     }
 
     private Response handleUnexpectedException(Exception ex) {
-        logger.error("Failed to get ApiClient from idm due to an unexpected exception", ex);
+        LOGGER.error("Failed to get ApiClient from idm due to an unexpected exception", ex);
         return new Response(Status.INTERNAL_SERVER_ERROR);
     }
 
     /**
-     * Responsible for creating the {@link AuthorizeResponseFetchApiClientFilter}
+     * Helper function capable of retrieving the client_id parameter from the Request's Query params.
      *
-     * Mandatory config:
-     * - idmGetApiClientBaseUri: the base uri used to build the IDM query to get the apiClient, the client_id is expected
-     * to be appended to this uri (and some query params).
-     * - clientHandler: the clientHandler to use to call out to IDM (must be configured with the credentials required to
-     * query IDM)
-     *
-     * Example config:
-     * {
-     *           "comment": "Add ApiClient data to the context attributes for the AS /authorize route",
-     *           "name": "AuthoriseResponseFetchApiClientFilter",
-     *           "type": "AuthoriseResponseFetchApiClientFilter",
-     *           "config": {
-     *             "idmGetApiClientBaseUri": "https://&{identity.platform.fqdn}/openidm/managed/apiClient",
-     *             "clientHandler": "IDMClientHandler"
-     *            }
-     * }
+     * @return Promise<String, NeverThrowsException> which returns the client_id or null if it does not exist
      */
-    public static class Heaplet extends BaseFetchApiClientHeaplet {
-        @Override
-        public Object create() throws HeapException {
-            return new AuthorizeResponseFetchApiClientFilter(createApiClientService());
-        }
+    static Function<Request, Promise<String, NeverThrowsException>> queryParamClientIdRetriever() {
+        return request -> {
+            final List<String> clientIdParams = request.getQueryParams().get("client_id");
+            if (clientIdParams != null && clientIdParams.size() > 0) {
+                return Promises.newResultPromise(clientIdParams.get(0));
+            } else {
+                return Promises.newResultPromise(null);
+            }
+        };
+    }
+
+    /**
+     * Helper function capable of retrieving the client_id parameter from the Request's Form entity.
+     *
+     * @return Promise<String, NeverThrowsException> which returns the client_id or null if it does not exist
+     */
+    static Function<Request, Promise<String, NeverThrowsException>> formClientIdRetriever() {
+        return request -> request.getEntity().getFormAsync()
+                .then(form -> form.getFirst("client_id"))
+                .thenCatch(ioe -> {
+                    LOGGER.warn("Failed to extract client_id from /par request due to exception", ioe);
+                    return null;
+                });
     }
 
 }

commit ed1823962e4dd5bd18947a9c0a98a9d68becafa5

1215: Fixing AuthorizeResponseFetchApiClientFilter alias to reference the Heaplet correctly
The route protecting /authorize was failing to load due to a bug introduced by the PR https://github.com/SecureApiGateway/secure-api-gateway-ob-uk/pull/467 which extracted the AuthorizeResponseFetchApiClientFilterHeaplet class.

Correcting a typo in the filter name alias to Authorise => Authorize to make the OAuth2.0 terminology.

https://github.com/SecureApiGateway/SecureApiGateway/issues/1215

secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/SecureApiGatewayClassAliasResolver.java
@@ -23,7 +23,7 @@ import org.forgerock.openig.alias.ClassAliasResolver;
 import com.forgerock.sapi.gateway.am.ReSignIdTokenFilter;
 import com.forgerock.sapi.gateway.common.exception.SapiLogAttachedExceptionFilterHeaplet;
 import com.forgerock.sapi.gateway.consent.ConsentRequestAccessAuthorisationFilter;
-import com.forgerock.sapi.gateway.dcr.idm.AuthorizeResponseFetchApiClientFilter;
+import com.forgerock.sapi.gateway.dcr.idm.AuthorizeResponseFetchApiClientFilterHeaplet;
 import com.forgerock.sapi.gateway.dcr.idm.FetchApiClientFilter;
 import com.forgerock.sapi.gateway.dcr.idm.ParResponseFetchApiClientFilterHeaplet;
 import com.forgerock.sapi.gateway.dcr.request.RegistrationRequestEntityValidatorFilter;
@@ -72,7 +72,7 @@ public class SecureApiGatewayClassAliasResolver implements ClassAliasResolver {
         ALIASES.put("ContextCertificateRetriever", ContextCertificateRetriever.class);
         ALIASES.put("HeaderCertificateRetriever", HeaderCertificateRetriever.class);
         ALIASES.put("RouteMetricsFilter", RouteMetricsFilter.class);
-        ALIASES.put("AuthoriseResponseFetchApiClientFilter", AuthorizeResponseFetchApiClientFilter.class);
+        ALIASES.put("AuthorizeResponseFetchApiClientFilter", AuthorizeResponseFetchApiClientFilterHeaplet.class);
         ALIASES.put("TokenEndpointMetricsContextSupplier", TokenEndpointMetricsContextSupplier.class);
         ALIASES.put("FapiAuthorizeRequestValidationFilter", FapiAuthorizeRequestValidationFilter.class);
         ALIASES.put("FapiParRequestValidationFilter", FapiParRequestValidationFilter.class);

secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/dcr/idm/AuthorizeResponseFetchApiClientFilterHeaplet.java
@@ -41,7 +41,7 @@ import com.forgerock.sapi.gateway.dcr.idm.FetchApiClientFilter.BaseFetchApiClien
  * }
  * }</pre>
  */
-public class AuthoriseResponseFetchApiClientFilterHeaplet extends BaseFetchApiClientHeaplet {
+public class AuthorizeResponseFetchApiClientFilterHeaplet extends BaseFetchApiClientHeaplet {
 
     @Override
     public Object create() throws HeapException {

secure-api-gateway-ig-extensions/src/test/java/com/forgerock/sapi/gateway/dcr/idm/AuthorizeResponseFetchApiClientFilterTest.java
@@ -64,7 +64,7 @@ class AuthorizeResponseFetchApiClientFilterTest extends BaseAuthorizeResponseFet
     public class HeapletTests {
         @Test
         void failsToConstructIfClientHandlerIsMissing() {
-            final HeapException heapException = assertThrows(HeapException.class, () -> new AuthoriseResponseFetchApiClientFilterHeaplet().create(Name.of("test"),
+            final HeapException heapException = assertThrows(HeapException.class, () -> new AuthorizeResponseFetchApiClientFilterHeaplet().create(Name.of("test"),
                     json(object()), new HeapImpl(Name.of("heap"))), "Invalid object declaration");
             assertEquals(heapException.getCause().getMessage(), "/clientHandler: Expecting a value");
         }
@@ -75,7 +75,7 @@ class AuthorizeResponseFetchApiClientFilterTest extends BaseAuthorizeResponseFet
             final HeapImpl heap = new HeapImpl(Name.of("heap"));
             heap.put("idmClientHandler", idmClientHandler);
 
-            assertThrows(JsonValueException.class, () -> new AuthoriseResponseFetchApiClientFilterHeaplet().create(Name.of("test"),
+            assertThrows(JsonValueException.class, () -> new AuthorizeResponseFetchApiClientFilterHeaplet().create(Name.of("test"),
                     json(object(field("clientHandler", "idmClientHandler"))), heap), "/idmGetApiClientBaseUri: Expecting a value");
         }
 
@@ -89,7 +89,7 @@ class AuthorizeResponseFetchApiClientFilterTest extends BaseAuthorizeResponseFet
 
             final JsonValue config = json(object(field("clientHandler", "idmClientHandler"),
                     field("idmGetApiClientBaseUri", idmBaseUri)));
-            final AuthorizeResponseFetchApiClientFilter filter = (AuthorizeResponseFetchApiClientFilter) new AuthoriseResponseFetchApiClientFilterHeaplet().create(Name.of("test"), config, heap);
+            final AuthorizeResponseFetchApiClientFilter filter = (AuthorizeResponseFetchApiClientFilter) new AuthorizeResponseFetchApiClientFilterHeaplet().create(Name.of("test"), config, heap);
 
             // Test the filter created by the Heaplet
             callFilterValidateSuccessBehaviour(idmApiClientData, filter);